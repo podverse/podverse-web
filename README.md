d25a3c8dbc8c98f676c8ec20d6e9a0a4a6755972
Mitch Downey <mitchldowney@gmail.com>
2016-01-01 15:46:19 -0600

    Create NPM / Express / Nunjucks / Bootstrap hello world example

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # podverse-web
 Web app for the Podverse podcast clip sharing system
+
+---
+
+npm install
+
+node app.js
+
+open localhost:9000 in your browser
---
+++ app.js
@@ -0,0 +1,30 @@
+var express   = require('express'),
+    app       = express(),
+    nunjucks  = require('nunjucks');
+
+var port = process.env.PORT || 9000;
+
+var _templates = process.env.NODE_PATH ? process.env.NODE_PATH + '/templates' : 'templates' ;
+
+nunjucks.configure(_templates, {
+  autoescape: true,
+  cache: false,
+  express: app
+});
+
+app.engine('html', nunjucks.render);
+app.set('view engine', 'html');
+
+// app.use(express.static(__dirname + '/public'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+
+app.get('/', function(req, res) {
+  res.render('index.html');
+})
+
+app.get('/:page', function(req, res) {
+  res.render(req.params.page);
+});
+
+app.listen(port);
+console.log('Listening on port %s...', port);
