ba3455a9cfafb55b5a2b9804067c877a6f2b768f
Mitch Downey <mitchldowney@gmail.com>
2016-01-02 04:35:11 -0600

    Add mediaeleement.js; Handle Chrome crashes when seeking

--- libs/js/scripts.js
+++ libs/js/scripts.js
@@ -1,5 +1,7 @@
 'use strict'
 
+
+
 // Dan Carlin Hardcore History - RSS Feed - http://feeds.feedburner.com/dancarlin/history?format=xml
 
 var dchhEpisode = 'http://traffic.libsyn.com/dancarlinhh/dchha56_Kings_of_Kings.mp3';
@@ -8,19 +10,76 @@ var waitwaitEpisode = 'http://play.podtrac.com/npr-344098539/npr.mc.tritondigita
 var jreEpisode = 'http://traffic.libsyn.com/joeroganexp/p564.mp3';
 var dtfhEpisode = 'http://traffic.libsyn.com/lavenderhour/DTFH_178_zach_leary.mp3';
 
-var audio = document.createElement('audio');
-audio.setAttribute('controls', '');
-audio.setAttribute('preload', 'metadata');
-audio.setAttribute('id', 'open-player');
+var restartAttempts = 0;
+var lastPlaybackPosition = -1;
 
-var source = document.createElement('source');
-source.setAttribute('src', '');
-audio.appendChild(source);
+var startTime = "500";
+var endTime = "510";
 
-var playerContainer = document.getElementById('player-container');
-playerContainer.appendChild(audio);
+var createAndAppendAudio = function() {
+  var audio = document.createElement('audio');
+  audio.setAttribute('src', dtfhEpisode);
+  audio.setAttribute('type', 'audio/mpeg');
+  audio.setAttribute('codecs', 'mp3');
+  audio.preload = "metadata";
+  // var source = document.createElement('source');
+  // source.setAttribute('src', dtfhEpisode);
+  // audio.appendChild(source);
+  $('#player').append(audio);
+  $('audio').mediaelementplayer();
 
-// audio.load();
-// audio.onloadedmetadata = function() {
-//     audio.play();
-// };
+  audio.onloadedmetadata = function() {
+    // If the lastPlaybackPosition is greater than -1, then the audio player must
+    // have crashed and then restarted, and we should resume from the last saved
+    // playback position. Else begin from the clip start time.
+    if (lastPlaybackPosition > -1) {
+      audio.currentTime = lastPlaybackPosition;
+    } else {
+      audio.currentTime = startTime;
+    }
+  }
+
+  audio.onseeking = function() {
+    console.log('is seeking');
+    audio.pause();
+    audio.play();
+  }
+
+  audio.ontimeupdate = function() {
+    lastPlaybackPosition = audio.currentTime;
+  }
+
+  audio.onerror = function(e) {
+    switch (e.target.error.code) {
+      case e.target.error.MEDIA_ERR_ABORTED:
+        console.log('Aborted the video playback.');
+        break;
+      // Chrome will frequently throw a MEDIA_ERR_NETWORK error and crash when seeking
+      // to a position in a clip. I have encountered the issue with native HTML 5 Chrome
+      // components, as well as with 3rd party libraries like JWPlayer and MediaElement.
+      // The only work around for this bug I have found is to listen for the error,
+      // then remove the <audio> element and recreate and append the audio element.
+      case e.target.error.MEDIA_ERR_NETWORK:
+        console.log('A network error caused the audio download to fail.');
+        if (restartAttempts < 5) {
+          restartAttempts++;
+          $('#player').empty();
+          createAndAppendAudio();
+        }
+        break;
+      case e.target.error.MEDIA_ERR_DECODE:
+        console.log('The audio playback was aborted due to a corruption problem or because the video used features your browser did not support.');
+        break;
+      case e.target.error.MEDIA_ERR_SRC_NOT_SUPPORTED:
+        console.log('The video audio not be loaded, either because the server or network failed or because the format is not supported.');
+        break;
+      default:
+        console.log('An unknown error occurred.');
+        break;
+    }
+  }
+}
+
+$(document).ready(function() {
+  createAndAppendAudio();
+});
