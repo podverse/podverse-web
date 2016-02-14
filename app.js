62f156b627ec1a89d7d6a39e6f33123a98b72266
Mitch Downey <mitchldowney@gmail.com>
2016-02-13 23:01:53 -0600

    Add seconds to HHMMSS conversion filter and function

--- app.js
+++ app.js
@@ -37,9 +37,46 @@ var env = nunjucks.configure(__dirname + '/templates', {
 
 env.addFilter('stringify', function(str) {
   var s = JSON.stringify(str);
-  s = s.replace(/&quot;/g,'"');
   return s
 });
+
+// TODO: This identify function is also in the scripts.js. Probably this should
+// be refactored somehow.
+env.addFilter('convertSecToHHMMSS', function(sec) {
+    // thanks to dkreuter http://stackoverflow.com/questions/1322732/convert-seconds-to-hh-mm-ss-with-javascript
+    var totalSec = sec;
+
+    var hours = parseInt(totalSec / 3600) % 24;
+    var minutes = parseInt(totalSec / 60) % 60;
+    var seconds = totalSec % 60;
+    var result = '';
+
+    if (hours > 0) {
+      result += hours + ':';
+    }
+
+    if (minutes > 9) {
+      result += minutes + ':';
+    } else if (minutes > 0 && hours > 0) {
+      result += '0' + minutes + ':';
+    } else if (minutes > 0){
+      result += minutes + ':';
+    }
+
+    if (seconds > 9) {
+      result += seconds;
+    } else if (seconds > 0 && minutes > 0) {
+      result += '0' + minutes + ':';
+    } else if (seconds > 0) {
+      result += seconds;
+    } else {
+      result += '00';
+    }
+
+    return result
+})
+
+
 
 app.use('/bower_components', feathers.static(__dirname + '/bower_components'));
 app.use('/libs', feathers.static(__dirname + '/libs'));
--- libs/js/scripts.js
+++ libs/js/scripts.js
@@ -1,5 +1,39 @@
 'use strict';
 
+var convertSecToHHMMSS = function(sec) {
+  // thanks to dkreuter http://stackoverflow.com/questions/1322732/convert-seconds-to-hh-mm-ss-with-javascript
+  var totalSec = sec;
+
+  var hours = parseInt(totalSec / 3600) % 24;
+  var minutes = parseInt(totalSec / 60) % 60;
+  var seconds = totalSec % 60;
+  var result = '';
+
+  if (hours > 0) {
+    result += hours + ':';
+  }
+
+  if (minutes > 9) {
+    result += minutes + ':';
+  } else if (minutes > 0 && hours > 0) {
+    result += '0' + minutes + ':';
+  } else if (minutes > 0){
+    result += minutes + ':';
+  }
+
+  if (seconds > 9) {
+    result += seconds;
+  } else if (seconds > 0 && minutes > 0) {
+    result += '0' + minutes + ':';
+  } else if (seconds > 0) {
+    result += seconds;
+  } else {
+    result += '00';
+  }
+
+  return result
+}
+
 // Podcast / Episode / Clip variables added to the window
 // object in player.html
 
@@ -46,7 +80,7 @@ var setPlayerInfo = function() {
     $('#player-podcast-title').html(podcastTitle);
     $('#player-sub-title').html(episodeTitle);
     $('#player-title').html(clipTitle);
-    $('#player-stats-duration').html('Clip:' + duration + ' - ' + startTime + ' to ' + endTime);
+    $('#player-stats-duration').html('Clip: ' + convertSecToHHMMSS(duration) + ' - ' + convertSecToHHMMSS(startTime) + ' to ' + convertSecToHHMMSS(endTime));
     $('#player-stats-listens').html('Listens: 1234');
     $('#player-restart-clip').html('Restart Clip');
     $('#player-restart-clip').css('display', 'block');
