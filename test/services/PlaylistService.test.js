3c93109f9578eafa7097585697341cf1da8534df
Vincent Schramer <vinciple@gmail.com>
2016-07-12 13:18:03 -0500

    Move everything around, clean the slate a little bit

---
+++ test/services/PlaylistService.test.js
@@ -0,0 +1,9 @@
+
+const PlaylistService = require('services/playlists/PlaylistService.js');
+
+
+describe('PlaylistService', function () {
+  it('should work', function () {
+    this.playlistSvc = new PlaylistService();
+  });
+});
