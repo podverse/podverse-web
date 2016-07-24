9a353d599c792ff529cf591bbef17792a624fc1f
Mitch Downey <mitchldowney@gmail.com>
2016-07-24 02:24:08 -0500

    Add tests for processJWTIfExists

--- test/middleware/processJWTIfExists.js
+++ test/middleware/processJWTIfExists.js
@@ -1,15 +1,59 @@
 const processJWTIfExists = require('middleware/auth/processJWTIfExists.js');
+const appFactory = require('appFactory.js')
 const {createValidTestJWT} = require('test/helpers.js');
 const config = require('config.js');
 
 describe('processJWTIfExists Middleware', function () {
 
-  describe('when there is a JWT in the header', function () {
+  beforeEach(function () {
+    this.app = appFactory();
+
+    this.app
+      .get('/test-jwt', function (req, res) {
+        if (req.feathers.userId === undefined) {
+          res.status(600, 'No authorization header was provided.').send();
+        } else {
+          res.status(200).send(req.feathers);
+        }
+      });
+
+      this.request = chai.request(this.app)
+        .get('/test-jwt');
+  });
+
+  describe('when there is undefined in the authorization header', function () {
+
+    beforeEach(function (done) {
+      this.request
+        .end((err, res) => {
+          this.response = res;
+          done();
+        });
+    });
+
+    it('should immediately call next()', function () {
+      expect(this.response).to.have.status(600);
+    });
+
+  });
+
+  describe('when there is a JWT in the authorization header', function () {
 
     describe('when the JWT is valid', function () {
 
-      xit('should set the user identifier on req.feathers.userId', function () {
+      beforeEach(function (done) {
+        this.validToken = createValidTestJWT();
 
+        this.request
+          .set('Authorization', this.validToken)
+          .end((err, res) => {
+            this.response = res;
+            done();
+          });
+      });
+
+      it('should set the user identifier on req.feathers.userId', function () {
+        expect(this.response.body.userId).to.equal('curly@podverse.fm');
       });
 
     });
@@ -22,8 +66,19 @@ describe('processJWTIfExists Middleware', function () {
       // method, but swap out the config.apiSecret it uses with a wrong apiSecret value?
       // If yes, would we want to do that here?
 
-      xit('should throw an unauthorized error', function () {
+      beforeEach(function (done) {
+        this.invalidToken = 'the price is wrong bob';
 
+        this.request
+          .set('Authorization', this.invalidToken)
+          .end((err, res) => {
+            this.response = res;
+            done();
+          });
+      });
+
+      it('should throw an unauthorized error', function () {
+        expect(this.response.status).to.equal(401);
       });
     });
 
