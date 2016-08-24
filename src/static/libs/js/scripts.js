0897257e0f648fd1dc269de0476e6f31859ade80
Mitch Downey <mitchldowney@gmail.com>
2016-08-24 00:20:02 -0500

    Messy fixes for browser errors

--- src/static/libs/js/scripts.js
+++ src/static/libs/js/scripts.js
@@ -1,6 +1,11 @@
 /*eslint-disable*/
 'use strict';
 
+// Set default values for vars that handle player crashes and autoplay functionality
+window.restartAttempts = 0;
+window.lastPlaybackPosition = -1;
+window.endTimeHasBeenReached = false;
+
 var convertSecToHHMMSS = function(sec) {
   // thanks to dkreuter http://stackoverflow.com/questions/1322732/convert-seconds-to-hh-mm-ss-with-javascript
   var totalSec = sec;
@@ -48,9 +53,14 @@ var loadmediaRef = function(index, shouldPlay) {
   // Only clips have a startTime and endTime
   startTime = item.startTime;
   endTime = item.endTime;
-  isClip = typeof startTime !== "undefined" ? true : false;
 
-  if (isClip === true) {
+  if (window.startTime === 0 && window.endTime !== null) {
+    isEpisode = true;
+  } else {
+    isEpisode = false;
+  }
+
+  if (isEpisode === false) {
     clipTitle = item.title;
     duration = item.duration;
     podcastTitle = item.episode.podcast.title;
@@ -61,12 +71,11 @@ var loadmediaRef = function(index, shouldPlay) {
   } else { // handle item as episode
     clipTitle = "";
     duration = item.duration;
-    podcastTitle = item.podcast.title;
-    podcastImageURL = item.podcast.imageURL;
-    episodeTitle = item.title;
-    episodeMediaURL = item.mediaURL;
-    episodePubDate = item.pubDate;
-
+    podcastTitle = item.episode.podcast.title;
+    podcastImageURL = item.episode.podcast.imageURL;
+    episodeTitle = item.episode.title;
+    episodeMediaURL = item.episode.mediaURL;
+    episodePubDate = item.episode.pubDate;
     startTime = "0";
     endTime = item.duration;
   }
@@ -81,126 +90,131 @@ var loadmediaRef = function(index, shouldPlay) {
 }
 
 var setPlayerInfo = function() {
-  // Insert content into template differently depending on
-  // whether the item is a clip or an episode
-  isClip = false;
 
-  if (typeof startTime !== "undefined" && startTime > 0) {
-    isClip = true;
+  if (window.isPlayerPage) {
+
+    if (window.startTime === 0 && window.endTime !== null) {
+      isEpisode = true;
+    } else {
+      isEpisode = false;
+    }
+
+    if (isEpisode === false) {
+      $('#player-image img').attr('src', podcastImageURL);
+      $('#player-restart-clip').css('display', 'block');
+      $('#player-title').css('padding', '0.75rem 0 1rem 0');
+      $('#player-podcast-title').html(podcastTitle);
+      $('#player-sub-title').html(episodeTitle);
+      $('#player-condensed-title').html(clipTitle);
+      $('#player-title').html(clipTitle);
+      $('#player-stats-duration').html('Clip: ' + convertSecToHHMMSS(duration) + ' - ' + convertSecToHHMMSS(startTime) + ' to ' + convertSecToHHMMSS(endTime));
+      $('#player-stats-listens').html('Listens: 1234');
+      $('#player-restart-clip').html('Restart Clip');
+    } else {
+      $('#player-restart-clip').css('display', 'none');
+      $('#player-title').css('padding', '0.5rem 0 0.75rem 0');
+      $('#player-image img').attr('src', podcastImageURL);
+      $('#player-podcast-title').html(podcastTitle);
+      $('#player-sub-title').html(episodeTitle);
+      $('#player-condensed-title').html(episodeTitle);
+      $('#player-title').html('');
+      $('#player-stats-duration').html('Full Episode: ' + readableDate(episodePubDate));
+      $('#player-stats-listens').html('Listens: 1234');
+    }
+
+    window.restartAttempts = 0;
+    window.lastPlaybackPosition = -1;
+    window.endTimeHasBeenReached = false;
   }
-
-  if (isClip === true) {
-    $('#player-image img').attr('src', podcastImageURL);
-    $('#player-restart-clip').css('display', 'block');
-    $('#player-title').css('padding', '0.75rem 0 1rem 0');
-    $('#player-podcast-title').html(podcastTitle);
-    $('#player-sub-title').html(episodeTitle);
-    $('#player-condensed-title').html(clipTitle);
-    $('#player-title').html(clipTitle);
-    $('#player-stats-duration').html('Clip: ' + convertSecToHHMMSS(duration) + ' - ' + convertSecToHHMMSS(startTime) + ' to ' + convertSecToHHMMSS(endTime));
-    $('#player-stats-listens').html('Listens: 1234');
-    $('#player-restart-clip').html('Restart Clip');
-  } else {
-    $('#player-restart-clip').css('display', 'none');
-    $('#player-title').css('padding', '0.5rem 0 0.75rem 0');
-    $('#player-image img').attr('src', podcastImageURL);
-    $('#player-podcast-title').html(podcastTitle);
-    $('#player-sub-title').html(episodeTitle);
-    $('#player-condensed-title').html(episodeTitle);
-    $('#player-title').html('');
-    $('#player-stats-duration').html('Full Episode: ' + readableDate(episodePubDate));
-    $('#player-stats-listens').html('Listens: 1234');
-  }
-
-  window.restartAttempts = 0;
-  window.lastPlaybackPosition = -1;
-  window.endTimeHasBeenReached = false;
 }
 
 setPlayerInfo();
 
 var createAndAppendAudio = function() {
 
-  // If audio player elements are already on the page, remove them first.
-  $('.mejs-offscreen').remove();
-  $('.mejs-container').remove();
+  if (isPlayerPage) {
 
-  window.audio = document.createElement('audio');
-  audio.setAttribute('src', episodeMediaURL);
-  audio.setAttribute('type', 'audio/mpeg');
-  audio.setAttribute('codecs', 'mp3');
-  audio.preload = "auto";
-  $('#player').append(audio);
+    // If audio player elements are already on the page, remove them first.
+    $('.mejs-offscreen').remove();
+    $('.mejs-container').remove();
 
-  $('audio').mediaelementplayer({
-    alwaysShowHours: true
-  });
+    window.audio = document.createElement('audio');
+    audio.setAttribute('src', episodeMediaURL);
+    audio.setAttribute('type', 'audio/mpeg');
+    audio.setAttribute('codecs', 'mp3');
+    audio.preload = "auto";
+    $('#player').append(audio);
 
-  audio.onloadedmetadata = function() {
-    // If the lastPlaybackPosition is greater than -1, then the audio player must
-    // have crashed and then restarted, and we should resume from the last saved
-    // playback position. Else begin from the clip start time.
-    if (lastPlaybackPosition > -1) {
-      audio.currentTime = lastPlaybackPosition;
-    } else {
-      audio.currentTime = startTime || 0;
+    $('audio').mediaelementplayer({
+      alwaysShowHours: true
+    });
+
+    audio.onloadedmetadata = function() {
+      // NOTE: If the lastPlaybackPosition is greater than -1, then the audio player must
+      // have crashed and then restarted, and we should resume from the last saved
+      // playback position. Else begin from the clip start time.
+      if (lastPlaybackPosition > -1) {
+        audio.currentTime = lastPlaybackPosition;
+      } else {
+        audio.currentTime = startTime || 0;
+      }
+    };
+
+    audio.oncanplay = function() {
+      var autoplay = $.cookie('autoplay');
+      if (autoplay === 'On') {
+        audio.play();
+      }
     }
-  };
 
-  audio.oncanplay = function() {
-    var autoplay = $.cookie('autoplay');
-    if (autoplay === 'On') {
-      audio.play();
-    }
+    audio.ontimeupdate = function() {
+      // Stop the clip once when the end time has been reached
+      if (Math.floor(audio.currentTime) == endTime && endTimeHasBeenReached == false) {
+          endTimeHasBeenReached = true;
+          audio.pause();
+      }
+
+      // Skip to start time once when the user first hits play on mobile devices
+      if (lastPlaybackPosition == -1) {
+          audio.currentTime = startTime;
+      }
+
+      // TODO: Can this be made more efficient than rewriting the lastPlaybackPosition
+      // whenever time updates?
+      lastPlaybackPosition = audio.currentTime;
+    };
+
+    audio.onerror = function(e) {
+      switch (e.target.error.code) {
+        case e.target.error.MEDIA_ERR_ABORTED:
+          console.log('Aborted the video playback.');
+          break;
+        // Chrome will frequently throw a MEDIA_ERR_NETWORK error and crash when seeking
+        // to a position in a clip. I have encountered the issue with native HTML 5 Chrome
+        // components, as well as with 3rd party libraries like JWPlayer and MediaElement.
+        // The only work around for this bug I have found is to listen for the error,
+        // then remove the <audio> element and recreate and append the audio element.
+        // TODO: Is there a better work around or resolution for the Chrome bug?
+        case e.target.error.MEDIA_ERR_NETWORK:
+          console.log('A network error caused the audio download to fail.');
+          if (restartAttempts < 5) {
+            restartAttempts++;
+            $('#player').empty();
+            createAndAppendAudio();
+          }
+          break;
+        case e.target.error.MEDIA_ERR_DECODE:
+          console.log('The audio playback was aborted due to a corruption problem or because the video used features your browser did not support.');
+          break;
+        case e.target.error.MEDIA_ERR_SRC_NOT_SUPPORTED:
+          console.log('The video audio not be loaded, either because the server or network failed or because the format is not supported.');
+          break;
+        default:
+          console.log('An unknown error occurred.');
+          break;
+      }
+    };
   }
-
-  audio.ontimeupdate = function() {
-    // Stop the clip once when the end time has been reached
-    if (Math.floor(audio.currentTime) == endTime && endTimeHasBeenReached == false) {
-        endTimeHasBeenReached = true;
-        audio.pause();
-    }
-
-    // Skip to start time once when the user first hits play on mobile devices
-    if (lastPlaybackPosition == -1) {
-        audio.currentTime = startTime;
-    }
-
-    // TODO: Can this be made more efficient than rewriting the lastPlaybackPosition
-    // whenever time updates?
-    lastPlaybackPosition = audio.currentTime;
-  };
-
-  audio.onerror = function(e) {
-    switch (e.target.error.code) {
-      case e.target.error.MEDIA_ERR_ABORTED:
-        console.log('Aborted the video playback.');
-        break;
-      // Chrome will frequently throw a MEDIA_ERR_NETWORK error and crash when seeking
-      // to a position in a clip. I have encountered the issue with native HTML 5 Chrome
-      // components, as well as with 3rd party libraries like JWPlayer and MediaElement.
-      // The only work around for this bug I have found is to listen for the error,
-      // then remove the <audio> element and recreate and append the audio element.
-      // TODO: Is there a better work around or resolution for the Chrome bug?
-      case e.target.error.MEDIA_ERR_NETWORK:
-        console.log('A network error caused the audio download to fail.');
-        if (restartAttempts < 5) {
-          restartAttempts++;
-          $('#player').empty();
-          createAndAppendAudio();
-        }
-        break;
-      case e.target.error.MEDIA_ERR_DECODE:
-        console.log('The audio playback was aborted due to a corruption problem or because the video used features your browser did not support.');
-        break;
-      case e.target.error.MEDIA_ERR_SRC_NOT_SUPPORTED:
-        console.log('The video audio not be loaded, either because the server or network failed or because the format is not supported.');
-        break;
-      default:
-        console.log('An unknown error occurred.');
-        break;
-    }
-  };
 };
 
 $('#player-restart-clip').on('click', function() {
@@ -248,21 +262,24 @@ $('.playlist-item').on('click', function() {
   loadmediaRef(index, true);
 });
 
-var topOfPlayer = $("#player").offset().top;
-var heightOfPlayer = $("#player").outerHeight();
-var bottomOfPlayer = topOfPlayer + heightOfPlayer;
+if (isPlayerPage) {
+  var topOfPlayer = $("#player").offset().top;
+  var heightOfPlayer = $("#player").outerHeight();
+  var bottomOfPlayer = topOfPlayer + heightOfPlayer;
 
-var topOfPlayerContainer = $('#player-container').offset().top;
-var heightOfPlayerContainer = $('#player-container').outerHeight() - 27; // Subtract to prevent page content from jumping
-var bottomOfPlayerContainer = topOfPlayerContainer + heightOfPlayerContainer;
+  var topOfPlayerContainer = $('#player-container').offset().top;
+  var heightOfPlayerContainer = $('#player-container').outerHeight() - 27; // Subtract to prevent page content from jumping
+  var bottomOfPlayerContainer = topOfPlayerContainer + heightOfPlayerContainer;
 
-$(window).scroll(function(){
-    if($(window).scrollTop() > (bottomOfPlayer)){
-       $("#player-container").addClass('player-condensed');
-       $('html').attr('style', 'padding-top: ' + bottomOfPlayerContainer + 'px;' );
-    }
-    else{
-       $("#player-container").removeClass('player-condensed');
-       $('html').attr('style', '');
-    }
-});
+  $(window).scroll(function(){
+      if($(window).scrollTop() > (bottomOfPlayer)){
+         $("#player-container").addClass('player-condensed');
+         $('html').attr('style', 'padding-top: ' + bottomOfPlayerContainer + 'px;' );
+      }
+      else{
+         $("#player-container").removeClass('player-condensed');
+         $('html').attr('style', '');
+      }
+  });
+
+}
