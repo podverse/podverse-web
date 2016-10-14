0d71460e97790979fcde67eba2df25fccea34ef5
Mitch Downey <mitchldowney@gmail.com>
2016-10-13 23:45:00 -0500

    Add modal callbacks; modal UX improvements

--- src/static/libs/js/playlister.js
+++ src/static/libs/js/playlister.js
@@ -19,7 +19,12 @@ function togglePlaylistWidget () {
   $('#add-to-playlist').toggle();
 }
 
-function createPlaylist (params) {
+function createPlaylist (params, callback) {
+  if (params.title === '') {
+    alert('please provide a playlist title');
+    return false;
+  }
+
   $.ajax({
     type: 'POST',
     url: '/playlists/',
@@ -32,7 +37,9 @@ function createPlaylist (params) {
       isRecommendation: params.isRecommendation
     },
     success: function (response) {
-      alert('done!');
+      if (callback) {
+        callback(response);
+      }
     },
     error: function (xhr, status, error) {
       // TODO: add more helpful error messaging
@@ -41,7 +48,7 @@ function createPlaylist (params) {
   });
 }
 
-function addToPlaylist (playlistId, mediaRefId) {
+function addToPlaylist (playlistId, mediaRefId, callback) {
   $.ajax({
     type: 'POST',
     url: '/playlists/' + playlistId + '/addItem/' + mediaRefId,
@@ -49,7 +56,9 @@ function addToPlaylist (playlistId, mediaRefId) {
       Authorization: $.cookie('idToken')
     },
     success: function (response) {
-      alert('done!');
+      if (callback) {
+        callback(response);
+      }
     },
     error: function (xhr, status, error) {
       // TODO: add more helpful error messaging
@@ -58,119 +67,36 @@ function addToPlaylist (playlistId, mediaRefId) {
   });
 }
 
-// function makeClip () {
-//   event.preventDefault();
-//
-//   if (!isUUID(window.podcastId)) {
-//     alert('errrror');
-//     return;
-//   }
-//
-//   if (!isInt(window.episodeId)) {
-//     alert('errrror');
-//     return;
-//   }
-//
-//   $('#make-clip-start-time-error').hide();
-//   $('#make-clip-end-time-error').hide();
-//   $('#make-clip-start-time').removeClass('has-danger');
-//   $('#make-clip-end-time').removeClass('has-danger');
-//   $('#make-clip-time').removeClass('has-danger');
-//
-//   var startTime = $('#make-clip-start-time input').val(),
-//       endTime = $('#make-clip-end-time input').val();
-//
-//   if (startTime === '' && endTime === '') {
-//     $('#make-clip-start-time-error').html('Type a start time and/or end time to create a clip.');
-//     $('#make-clip-time').addClass('has-danger');
-//     $('#make-clip-start-time-error').show();
-//     return;
-//   }
-//
-//   if (!isHHMMSS(startTime) && startTime !== '') {
-//     $('#make-clip-start-time-error').html('Type time in hh:mm:ss format.');
-//     $('#make-clip-start-time').addClass('has-danger');
-//     $('#make-clip-start-time-error').show();
-//     return;
-//   }
-//
-//   if (!isHHMMSS(endTime) && endTime !== '') {
-//     $('#make-clip-end-time-error').html('Type time in hh:mm:ss format.');
-//     $('#make-clip-end-time').addClass('has-danger');
-//     $('#make-clip-end-time-error').show();
-//     return;
-//   }
-//
-//   startTime = convertHHMMSSToSeconds(startTime);
-//   endTime = convertHHMMSSToSeconds(endTime);
-//
-//   if (endTime === 0) {
-//     endTime = null;
-//   }
-//
-//   if (startTime === endTime) {
-//     $('#make-clip-start-time-error').html('Start time and end time cannot be the same.');
-//     $('#make-clip-start-time').addClass('has-danger');
-//     $('#make-clip-end-time').addClass('has-danger');
-//     $('#make-clip-start-time-error').show();
-//     return;
-//   }
-//
-//   if (endTime !== null && endTime <= startTime) {
-//     $('#make-clip-start-time-error').html('End time cannot be earlier than start time.');
-//     $('#make-clip-start-time').addClass('has-danger');
-//     $('#make-clip-end-time').addClass('has-danger');
-//     $('#make-clip-start-time-error').show();
-//     return;
-//   }
-//
-//   // TODO: how can we prevent malicious scripts in the description?
-//   var description = $('#make-clip-description textarea').val();
-//
-//   // TODO: set owner name based on ownerId
-//   var ownerName = 'random owner name';
-//
-//   // TODO: handle auth to post clip
-//   $.ajax({
-//     type: 'POST',
-//     url: '/clips',
-//     headers: {
-//       Authorization: $.cookie('idToken')
-//     },
-//     data: {
-//       episode: {
-//         id: episodeId,
-//         podcast: {
-//           id: podcastId
-//         }
-//       },
-//       startTime: startTime,
-//       endTime: endTime,
-//       description: description,
-//       ownerName: ownerName
-//     },
-//     success: function (response) {
-//       if (window.isPlayerPage) {
-//         toggleMakeClipWidget();
-//         $('#make-clip-start-time').val('');
-//         $('#make-clip-end-time').val('');
-//         $('#make-clip-description').val('');
-//         $('#clip-created-modal-link').val(location.protocol + '\/\/' + location.hostname + (location.port ? ':'+location.port: '')  + '\/clips\/' + response.id);
-//         $('#clip-created-modal').modal('show');
-//       } else {
-//         location.href = '\/clips\/' + response.id;
-//       }
-//     },
-//     error: function (xhr, status, error) {
-//       // TODO: add more helpful error messaging
-//       alert('errrror');
-//     }
-//
-//   });
-//
-//   // TODO: view my clips
-//
-//   // TODO: edit my clips
-//
-//   // TODO: add CSRF security
-// }
+function updatePlaylistItemCount(playlistId, total) {
+  $(".add-to-playlist-item[data-id=" + playlistId + "] .add-to-playlist-item-count")
+    .html("items: " + total);
+}
+
+function addNewPlaylistElement(playlist) {
+  var playlistId = playlist.id,
+      playlistTitle = playlist.title,
+      isRecommendation = playlist.isRecommendation;
+
+  var el = '<div class="add-to-playlist-item" data-id="' + playlistId +'">';
+      el +=   '<div class="add-to-playlist-item-count">';
+      el +=     'items: 0';
+      el +=   '</div>';
+      el +=   '<div class="add-to-playlist-item-title">';
+      el +=     playlistTitle
+      el +=   '</div>';
+      el += '</div> ';
+
+  if (isRecommendation) {
+    $(el).insertBefore($('#recommend .add-to-playlist-item').first());
+  } else {
+    $(el).insertBefore($('#add-to-playlist .add-to-playlist-item').first());
+  }
+
+  $(".add-to-playlist-item[data-id=" + playlistId + "]").on('click', function () {
+    var playlistId = $(this).data('id');
+    var mediaRefId = $('#player').data('id');
+    addToPlaylist(playlistId, mediaRefId, function (response) {
+      updatePlaylistItemCount(playlistId, response.mediaRefs.length);
+    });
+  });
+}
