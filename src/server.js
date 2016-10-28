1e11e46094b3d4c3b11fd7acd027eb22674e1612
Vincent Schramer <vinciple@gmail.com>
2016-10-28 10:18:23 -0500

    make server run on the configured port

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 
-const app = require('app.js');
-
-const port = 8080;
+const 
+    app = require('app.js'),
+    {port} = require('config.js');
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
