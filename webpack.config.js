dfe0ecd277565fdcd980d30f430b842ed6a0349a
Mitch Downey <mitchldowney@gmail.com>
2016-10-30 19:58:18 -0500

    Load tether variable for Bootstrap 4 dependency

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,36 @@
 /* Thanks to Ashwin Hariharan
 https://medium.freecodecamp.com/webpack-for-the-fast-and-the-furious-bf8d3746adbd#.4ijclp4kt */
 
-var webpack = require('webpack');
+var webpack = require("webpack");
 var path = require("path");
 
-var lib_dir = __dirname + '/src/static/libs',
-    node_dir = __dirname + '/node_modules';
+var lib_dir = __dirname + "/src/static/libs",
+    node_dir = __dirname + "/node_modules";
 
 var config = {
     resolve: {
         alias: {
-            bootstrap: node_dir + '/bootstrap/dist/js/bootstrap.min.js',
-            jquery: node_dir + '/jquery/dist/jquery.min.js',
-            jqueryCookie: node_dir + '/jquery.cookie/jquery.cookie.js',
-            mediaElement: node_dir + '/mediaelement/build/mediaelement-and-player.min.js',
-            mejsPlaybackRate: lib_dir + '/js/mejs_playbackrate_plugin.js',
-            tether: node_dir + '/tether/dist/js/tether.min.js',
-            truncate: lib_dir + '/vendors/truncate.min.js',
-            typeahead: node_dir + '/typeahead.js/dist/typeahead.bundle.min.js'
+            bootstrap: node_dir + "/bootstrap/dist/js/bootstrap.min.js",
+            jquery: node_dir + "/jquery/dist/jquery.min.js",
+            jqueryCookie: node_dir + "/jquery.cookie/jquery.cookie.js",
+            mediaElement: node_dir + "/mediaelement/build/mediaelement-and-player.min.js",
+            mejsPlaybackRate: lib_dir + "/js/mejs_playbackrate_plugin.js",
+            tether: node_dir + "/tether/dist/js/tether.min.js",
+            truncate: lib_dir + "/vendors/truncate.min.js",
+            typeahead: node_dir + "/typeahead.js/dist/typeahead.bundle.min.js"
         }
     },
 
     entry: {
-        app: ['./src/static/libs/js/index.js'],
-        vendors: ['bootstrap',
-                  'jquery',
-                  'jqueryCookie',
-                  'mediaElement',
-                  'mejsPlaybackRate',
-                  'tether',
-                  'truncate',
-                  'typeahead']
+        app: ["./src/static/libs/js/index.js"],
+        vendors: ["bootstrap",
+                  "jquery",
+                  "jqueryCookie",
+                  "mediaElement",
+                  "mejsPlaybackRate",
+                  "tether",
+                  "truncate",
+                  "typeahead"]
     },
 
     output: {
@@ -40,11 +40,12 @@ var config = {
 
     plugins: [
         new webpack.ProvidePlugin({
+            $: "jquery",
             jQuery: "jquery",
-            'window.jQuery': "jquery",
-            'window.$': "jquery"
+            "window.jQuery": "jquery",
+            "window.Tether": "tether"
         }),
-        new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js', Infinity)
+        new webpack.optimize.CommonsChunkPlugin("vendors", "vendors.js", Infinity)
     ]
 
     // module: {
