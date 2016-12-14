1a71edff70cc230392a97487d5f0f6ac74232ae2
Mitch Downey <mitchldowney@gmail.com>
2016-12-13 23:28:52 -0600

    Add missing comma

--- src/config.js
+++ src/config.js
@@ -13,7 +13,7 @@ module.exports = {
   auth0ClientId: process.env.auth0ClientId || 'tTGQrl5CenMDdpzcKNpmLIginyRgBJNN',
   auth0Domain: process.env.auth0Domain || 'mitchd.auth0.com',
 
-  googleAnalyticsUA: 'UA-87988078-1'
+  googleAnalyticsUA: 'UA-87988078-1',
 
   ga: {
     client_email: process.env.GA_CLIENT_EMAIL,
