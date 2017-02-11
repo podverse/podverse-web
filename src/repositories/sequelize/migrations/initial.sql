6c6bff555e9a3e9a676f4a8211054f8cc0d09ee3
Mitch Downey <mitchldowney@gmail.com>
2017-02-10 23:17:57 -0600

    Attempt to make migration on dev.podverse.fm work

--- src/repositories/sequelize/migrations/initial.sql
+++ src/repositories/sequelize/migrations/initial.sql
@@ -7,29 +7,27 @@
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
-SET row_security = off;
 
 --
--- Name: postgres; Type: COMMENT; Schema: -; Owner: postgres
+-- Name: podverse_web_dev; Type: COMMENT; Schema: -; Owner: postgres
 --
 
-COMMENT ON DATABASE postgres IS 'default administrative connection database';
+COMMENT ON DATABASE podverse_web_dev IS 'default administrative connection database';
 
 
 --
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner:
 --
 
 CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner:
 --
 
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
@@ -214,4 +212,3 @@ ALTER TABLE ONLY "subscribedPlaylists"
 --
 -- PostgreSQL database dump complete
 --
-
