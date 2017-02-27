4423748fde0d540a42d82f466e597c01ef575a70
Mitch Downey <mitchldowney@gmail.com>
2017-02-27 02:08:52 -0600

    Fix broken my playlists dropdown

--- src/static/libs/js/views/my-playlists/index.js
+++ src/static/libs/js/views/my-playlists/index.js
@@ -19,10 +19,10 @@ $('#myPlaylistsDropdown a').on('click', function () {
   } else if (selectedVal === 'My Playlists') {
     $('#playlist-list-my-playlists').show();
     sendGoogleAnalyticsEvent('Playlists', 'show My Playlists');
-  } else if (selectedVal === 'Recommended for Me') {
+  } else if (selectedVal === 'Shared with Me') {
     $('#playlist-list-recommended-for-me').show();
     sendGoogleAnalyticsEvent('Playlists', 'show Recommended for Me');
-  } else if (selectedVal === 'Recommended by Me') {
+  } else if (selectedVal === 'Shared by Me') {
     $('#playlist-list-recommended-by-me').show();
     sendGoogleAnalyticsEvent('Playlists', 'show Recommended by Me');
   }
