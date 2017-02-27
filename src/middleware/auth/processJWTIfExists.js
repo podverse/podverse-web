90fc4c61301c9fd6270821e3b4af6150e8006bec
Mitch Downey <mitchldowney@gmail.com>
2017-02-26 21:35:13 -0600

    Add temporary console logs to help track down an auth bug sometimes found in mobile iOS Safari

--- src/middleware/auth/processJWTIfExists.js
+++ src/middleware/auth/processJWTIfExists.js
@@ -11,20 +11,29 @@ function processJWTIfExists (req, res, next) {
 
   let token = req.headers['authorization'];
 
+  console.log('line-1 ', token);
+
   if (token == null) {
     let cookies = req.cookies;
     token = cookies.idToken;
   }
+
+  console.log('line-2', token);
 
   if (token == null) {
     next();
     return;
   }
 
+  console.log('line-3 ', token);
+
   try {
+    console.log('config', config);
     const verifiedJwt = nJwt.verify(token, config.jwtSigningKey);
     req.feathers.userId = verifiedJwt.body.sub;
   } catch(e) {
+    console.log(token);
+    console.log(e);
     next();
     return;
   }
