c44172cf89d75499c0076f664fe2dc346cb23f93
Mitch Downey <mitchldowney@gmail.com>
2017-03-01 01:55:39 -0600

    Update readableDate function

--- src/static/libs/js/utility.js
+++ src/static/libs/js/utility.js
@@ -51,22 +51,34 @@ export function convertSecToHHMMSS (sec) {
 }
 
 export function readableDate (date) {
-  // Thanks:) http://stackoverflow.com/questions/19485353/function-to-convert-timestamp-to-human-date-in-javascript
-  var dateObj = new Date(date),
-  year = dateObj.getFullYear(),
-  month = dateObj.getMonth() + 1,
-  day = dateObj.getDate();
+  if (typeof date === 'string' || typeof date === 'object') {
+    // Thanks:) http://stackoverflow.com/questions/19485353/function-to-convert-timestamp-to-human-date-in-javascript
+    var dateObj = new Date(date),
+        year = dateObj.getFullYear(),
+        month = dateObj.getMonth() + 1,
+        day = dateObj.getDate();
 
-  // If date is within the past 7 days, then display name of day instead of date
-  var date7DaysAgo = new Date().getTime() - (7 * 24 * 60 * 60 * 1000);
 
-  if (dateObj > date7DaysAgo) {
-    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    var dayName = days[dateObj.getDay()];
-    return dayName
-  } else {
-    // Else return the date in mm/dd/yyyy format
-    return month+'/'+day+'/'+year;
+    // If date is within the past 6 days, then display name of day instead of date
+    var date6DaysAgo = new Date().getTime() - (6 * 24 * 60 * 60 * 1000);
+    var today = new Date();
+    var yesterday = new Date(today);
+    yesterday.setDate(today.getDate() -1);
+
+    if (dateObj > date6DaysAgo) {
+      if (dateObj.getDay() === today.getDay()) {
+        return 'Today';
+      } else if (dateObj.getDay() === yesterday.getDay()) {
+        return 'Yesterday';
+      } else {
+        var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        var dayName = days[dateObj.getDay()];
+        return dayName
+      }
+    } else {
+      // Else return the date in mm/dd/yyyy format
+      return month+'/'+day+'/'+year;
+    }
   }
 }
 
