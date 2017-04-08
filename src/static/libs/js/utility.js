6ebda9d6f903863515fdd364800548f2d42918ee
Mitch Downey <mitchldowney@gmail.com>
2017-04-07 21:52:55 -0500

    Ensure sort query param is passed for pagination

--- src/static/libs/js/utility.js
+++ src/static/libs/js/utility.js
@@ -189,3 +189,16 @@ export function throttle (callback, limit) {
         }
     }
 }
+
+// Thanks jolly dot exe http://stackoverflow.com/a/901144/2608858
+export function getParameterByName(name, url) {
+    if (!url) {
+      url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
--- src/static/libs/js/views/home/index.js
+++ src/static/libs/js/views/home/index.js
@@ -3,19 +3,22 @@ require('../../auth.js');
 
 import { sendGoogleAnalyticsPageView,
          sendGoogleAnalyticsEvent } from '../../googleAnalytics.js';
+import { getParameterByName } from '../../utility.js';
 
+let sortParam = getParameterByName('sort');
+sortParam = sortParam ? 'sort=' + sortParam + '&' : '';
 
 if (showNextButton) {
   $('.pv-pagination-next').on('click', function () {
     sendGoogleAnalyticsEvent('Pagination', 'Next');
-    window.location = '/?page=' + (pageIndex + 1);
+    window.location = '/?' + sortParam + 'page=' + (pageIndex + 1);
   });
 }
 
 if (pageIndex > 1) {
   $('.pv-pagination-prev').on('click', function () {
     sendGoogleAnalyticsEvent('Pagination', 'Prev');
-    window.location = '/?page=' + (pageIndex - 1);
+    window.location = '/?' + sortParam + 'page=' + (pageIndex - 1);
   });
 }
 
