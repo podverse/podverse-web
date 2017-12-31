eb1f58d46bebe4706c5b35f85b21c786b2be1956
Mitch Downey <mitchldowney@gmail.com>
2017-12-31 02:58:15 -0600

    Kluge My Playlists to match My Podcasts layout

--- src/static/libs/js/views/my-playlists/index.js
+++ src/static/libs/js/views/my-playlists/index.js
@@ -6,30 +6,4 @@ import { sendGoogleAnalyticsPageView,
 
 $('img.lazy').lazyload();
 
-$('#myPlaylistsDropdown a').on('click', function () {
-  var selectedVal = $(this).html();
-
-  $('#playlist-list-my-playlists').hide();
-  $('#playlist-list-recommended-for-me').hide();
-  $('#playlist-list-recommended-by-me').hide();
-
-  if (selectedVal === 'All Playlists') {
-    $('#playlist-list-my-playlists').show();
-    $('#playlist-list-recommended-for-me').show();
-    $('#playlist-list-recommended-by-me').show();
-    sendGoogleAnalyticsEvent('Playlists', 'show All Playlists');
-  } else if (selectedVal === 'My Playlists') {
-    $('#playlist-list-my-playlists').show();
-    sendGoogleAnalyticsEvent('Playlists', 'show My Playlists');
-  } else if (selectedVal === 'Shared with Me') {
-    $('#playlist-list-recommended-for-me').show();
-    sendGoogleAnalyticsEvent('Playlists', 'show Recommended for Me');
-  } else if (selectedVal === 'Shared by Me') {
-    $('#playlist-list-recommended-by-me').show();
-    sendGoogleAnalyticsEvent('Playlists', 'show Recommended by Me');
-  }
-
-  $('#myPlaylistsDropdown button').html(selectedVal);
-});
-
 sendGoogleAnalyticsPageView();
