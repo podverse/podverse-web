1284d4998ee4dc3e157586e9ed220893c56a458a
Mitch Downey <mitchldowney@gmail.com>
2018-06-22 00:33:08 -0500

    Remove config defaults

--- src/config.js
+++ src/config.js
@@ -2,7 +2,8 @@
 
 const isCi = require('is-ci');
 
-const jwtSigningKeyB64 = process.env.jwtSigningKeyB64 || 'RhvV5pbdUOHL0Rns4mX9YgCZ23kvoLqlB6eFau-QIXkxjK4Tx4qFISuzCOg4lmR_';
+const jwtSigningKey = process.env.jwtSigningKey ? new Buffer(process.env.jwtSigningKey, 'base64') : '' ;
+
 const dbConfig = {
   host: process.env.DB_HOST || '127.0.0.1',
   port: process.env.DB_PORT || '5432',
@@ -18,17 +19,11 @@ module.exports = {
   host: process.env.HOST || '127.0.0.1',
   port: process.env.PORT || 8080,
   baseUrl: process.env.baseUrl || 'http://localhost:8080',
-  jwtSigningKey: new Buffer(jwtSigningKeyB64, 'base64'),
-  // TODO: change the auth0 values to podverse.fm's clientId and domain before deployment
-  auth0ClientId: process.env.auth0ClientId || 'tTGQrl5CenMDdpzcKNpmLIginyRgBJNN',
-  auth0Domain: process.env.auth0Domain || 'mitchd.auth0.com',
-
-  googleAnalyticsUA: (process.env.NODE_ENV === 'production') ? 'UA-87988078-1' : '',
-
+  jwtSigningKey,
+  auth0ClientId: process.env.auth0ClientId,
+  auth0Domain: process.env.auth0Domain,
+  googleAnalyticsUA: (process.env.NODE_ENV === 'production') ? '' : '',
   ga: {
-    client_email: process.env.GA_CLIENT_EMAIL,
-    private_key: process.env.GA_PRIVATE_KEY,
-    view_id: 'ga:134581781'
+    view_id: ''
   }
-
 };
