3401f5dc189a266aaf63cf6cc552ed3a971c1528
Mitch Downey <mitchldowney@gmail.com>
2019-01-23 03:09:27 -0600

    Setup env vars with docker namespaces

--- next.config.js
+++ next.config.js
@@ -5,7 +5,35 @@ const path = require('path')
 const webpack = require('webpack')
 const withImages = require('next-images')
 
+const envVars = {
+  NODE_ENV: process.env.NODE_ENV,
+  PROTOCOL: process.env.PROTOCOL,
+  PORT: process.env.PORT,
+  API_PROTOCOL: process.env.API_PROTOCOL,
+  API_PORT: process.env.API_PORT,
+  API_PATH: process.env.API_PATH,
+  API_VERSION: process.env.API_VERSION,
+  COOKIE_DOMAIN: process.env.COOKIE_DOMAIN,
+  COOKIE_PATH: process.env.COOKIE_PATH,
+  PAYPAL_ENV: process.env.PAYPAL_ENV,
+  PAYPAL_CLIENT_ID_PRODUCTION: process.env.PAYPAL_CLIENT_ID_PRODUCTION,
+  PAYPAL_CLIENT_ID_SANDBOX: process.env.PAYPAL_CLIENT_ID_SANDBOX,
+  GOOGLE_ANALYTICS_TRACKING_ID: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
+  DOMAIN: process.env.DOMAIN,
+  API_DOMAIN: process.env.API_DOMAIN,
+  PUBLIC_DOMAIN: process.env.PUBLIC_DOMAIN,
+  PUBLIC_API_DOMAIN: process.env.PUBLIC_API_DOMAIN
+}
+
 module.exports = withImages(withTypescript(withCss(withSass({
+  serverRuntimeConfig: {
+    ...envVars
+  },
+  publicRuntimeConfig: {
+    ...envVars,
+    DOMAIN: process.env.PUBLIC_DOMAIN,
+    API_DOMAIN: process.env.PUBLIC_API_DOMAIN
+  },
   webpack(config, options) {
     return {
       ...config,
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,28 +1,63 @@
-export const PROTOCOL = process.env.PROTOCOL
-export const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000
-export const DOMAIN = process.env.DOMAIN
+import getConfig from 'next/config'
+const { serverRuntimeConfig, publicRuntimeConfig } = getConfig()
+
+let env : any = {}
+
+env = {
+  NODE_ENV: publicRuntimeConfig.NODE_ENV,
+  PROTOCOL: publicRuntimeConfig.PROTOCOL,
+  PORT: publicRuntimeConfig.PORT,
+  API_PROTOCOL: publicRuntimeConfig.API_PROTOCOL,
+  API_PORT: publicRuntimeConfig.API_PORT,
+  API_PATH: publicRuntimeConfig.API_PATH,
+  API_VERSION: publicRuntimeConfig.API_VERSION,
+  COOKIE_DOMAIN: publicRuntimeConfig.COOKIE_DOMAIN,
+  COOKIE_PATH: publicRuntimeConfig.COOKIE_PATH,
+  PAYPAL_ENV: publicRuntimeConfig.PAYPAL_ENV,
+  PAYPAL_CLIENT_ID_PRODUCTION: publicRuntimeConfig.PAYPAL_CLIENT_ID_PRODUCTION,
+  PAYPAL_CLIENT_ID_SANDBOX: publicRuntimeConfig.PAYPAL_CLIENT_ID_SANDBOX,
+  GOOGLE_ANALYTICS_TRACKING_ID: publicRuntimeConfig.GOOGLE_ANALYTICS_TRACKING_ID,  
+}
+
+// For back-end use docker container namespaces,
+// for front-end use public domains.
+if (Object.keys(serverRuntimeConfig).length > 0) {
+  Object.assign(env, {
+    DOMAIN: serverRuntimeConfig.DOMAIN,
+    API_DOMAIN: serverRuntimeConfig.API_DOMAIN
+  })
+} else if (Object.keys(publicRuntimeConfig).length > 0) {
+  Object.assign(env, {
+    DOMAIN: publicRuntimeConfig.PUBLIC_DOMAIN,
+    API_DOMAIN: publicRuntimeConfig.PUBLIC_API_DOMAIN
+  })
+}
+
+export const PROTOCOL = env.PROTOCOL
+export const PORT = env.PORT ? parseInt(env.PORT, 10) : 3000
+export const DOMAIN = env.DOMAIN
 export const BASE_URL = `${PROTOCOL}://${DOMAIN}`
 
-export const API_PROTOCOL = process.env.API_PROTOCOL
-export const API_PORT = process.env.API_PORT ? parseInt(process.env.API_PORT, 10) : 1234
-export const API_DOMAIN = process.env.API_DOMAIN
-export const API_PATH = process.env.API_PATH
-export const API_VERSION = process.env.API_VERSION
+export const API_PROTOCOL = env.API_PROTOCOL
+export const API_PORT = env.API_PORT ? parseInt(env.API_PORT, 10) : 1234
+export const API_DOMAIN = env.API_DOMAIN
+export const API_PATH = env.API_PATH
+export const API_VERSION = env.API_VERSION
 export const API_BASE_URL = `${API_PROTOCOL}://${API_DOMAIN}/${API_PATH}/${API_VERSION}`
 
 export const cookieConfig = {
-  domain: process.env.COOKIE_DOMAIN ? process.env.COOKIE_DOMAIN : 'localhost',
-  path: process.env.COOKIE_PATH ? process.env.COOKIE_PATH : '/'
+  domain: env.COOKIE_DOMAIN ? env.COOKIE_DOMAIN : 'localhost',
+  path: env.COOKIE_PATH ? env.COOKIE_PATH : '/'
 }
 
 export const paypalConfig = {
-  env: process.env.PAYPAL_ENV,
-  production: process.env.PAYPAL_CLIENT_ID_PRODUCTION,
-  sandbox: process.env.PAYPAL_CLIENT_ID_SANDBOX
+  env: env.PAYPAL_ENV,
+  production: env.PAYPAL_CLIENT_ID_PRODUCTION,
+  sandbox: env.PAYPAL_CLIENT_ID_SANDBOX
 }
 
 export const metaDefaultImageUrl1200x630 = 'https://podverse.fm/static/images/podverse-logo-1200x630.png'
 
 export const googleAnalyticsConfig = {
-  trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID
+  trackingId: env.GOOGLE_ANALYTICS_TRACKING_ID
 }
