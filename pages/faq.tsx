2fe2620c7fb6c95787e8b1273143d463b620d770
Mitch Downey <mitchldowney@gmail.com>
2019-02-24 21:37:19 -0600

    Update faq.tsx

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -55,43 +55,12 @@ class FAQ extends Component<Props, State> {
         <h3>Table of Contents</h3>
         <ul>
           <li>
-            <a href='#edit-clips'>How do I edit my clips?</a>
-          </li>
-          <li>
-            <a href='#my-clips'>How do I find my clips?</a>
-          </li>
-          <li>
             <a href='#agplv3'>What does AGPLv3 mean?</a>
           </li>
           <li>
             <a href='#monopoly-proof'>How is Podverse monopoly-proof?</a>
           </li>
         </ul>
-        <hr />
-        <h3 id='edit-clips'>How do I edit my clips?</h3>
-        <p>
-          In order to edit a clip, <u>you must have been logged in when you created the clip</u>. After you create the clip, open it on the website or the iOS mobile app, and there will be an <i>Edit Clip</i> button on the right hand side of the page.
-        </p>
-        <p>
-          <b>iOS:</b> open the clip in the media player, then tap <i>Edit Clip</i>.
-        </p>
-        <div className='info-img'>
-          <img className='responsive' src='/static/images/edit-clip-ios.png' />
-        </div>
-        <hr />
-        <h3 id='my-clips'>How do I find my clips?</h3>
-        <p>
-          If you <u>are not logged in</u> when you create a clip, then you must save the link to the clip if you want to find it later.
-        </p>
-        <p>
-          If you <u>are logged in</u> when you create a clip, then you can find it in your My Clips list at any time on web or iOS.
-        </p>
-        <p>
-          <b>iOS:</b> Go to the Clips page, click the dropdown in the upperleft corner, then select My Clips.
-        </p>
-        <div className='info-img'>
-          <img className='responsive' src='/static/images/my-clips-ios.png' />
-        </div>
         <hr />
         <h3 id='agplv3'>What does AGPLv3 mean?</h3>
         <p>
