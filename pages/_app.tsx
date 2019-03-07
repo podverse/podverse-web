860acfd5c2c37a5246fcf06087c39f36e3dd9f2e
Mitch Downey <mitchldowney@gmail.com>
2019-03-06 18:07:38 -0600

    Fix settings UI checkboxes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -164,6 +164,11 @@ export default withRedux(initializeStore)(class MyApp extends App<Props> {
           type: actionTypes.SETTINGS_SET_HIDE_TIME_JUMP_BACKWARD_BUTTON,
           payload: 'false'
         })
+      } else {
+        ctx.store.dispatch({
+          type: actionTypes.SETTINGS_SET_HIDE_TIME_JUMP_BACKWARD_BUTTON,
+          payload: 'true'
+        })
       }
 
       if (parsedCookie.playbackSpeedButtonHide) {
--- pages/settings.tsx
+++ pages/settings.tsx
@@ -526,7 +526,7 @@ class Settings extends Component<Props, State> {
           <FormGroup check>
             <Label className='checkbox-label' check>
               <Input
-                checked={timeJumpBackwardButtonHide === 'true'}
+                checked={timeJumpBackwardButtonHide === 'true' || !timeJumpBackwardButtonHide}
                 onChange={this.handleToggleTimeJumpBackwardButton}
                 type="checkbox" />
               &nbsp;&nbsp;Hide jump backwards button
@@ -571,7 +571,7 @@ class Settings extends Component<Props, State> {
           <FormGroup check>
             <Label className='checkbox-label' check>
               <Input
-                checked={nsfwLabelsHide === 'true'}
+                checked={nsfwLabelsHide === 'true' || !nsfwLabelsHide}
                 onChange={this.handleToggleNSFWLabels}
                 type="checkbox" />
               &nbsp;&nbsp;Hide NSFW Labels
