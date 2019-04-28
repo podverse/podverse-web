5fd11ba15c660905590edb764cddd07c33702d59
Mitch Downey <mitchldowney@gmail.com>
2019-04-27 19:34:37 -0500

    Move auth/user methods into user service

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,7 +1,5 @@
 import axios from 'axios'
 import config from '~/config'
-import { NowPlayingItem } from '~/lib/nowPlayingItem'
-import { convertObjectToQueryString } from '~/lib/utility'
 const { API_BASE_URL } = config()
 
 export const getAuthenticatedUserInfo = async (bearerToken) => {
@@ -27,39 +25,6 @@ export const login = async (email: string, password: string) => {
 export const logOut = async () => {
   return axios(`${API_BASE_URL}/auth/logout`, {
     method: 'post',
-    withCredentials: true
-  })
-}
-
-export const getLoggedInUserMediaRefs = async (bearerToken, nsfwMode, sort = 'most-recent', page = 1) => {
-  let filteredQuery: any = {}
-  filteredQuery.sort = sort
-  filteredQuery.page = page
-  const queryString = convertObjectToQueryString(filteredQuery)
-
-  return axios(`${API_BASE_URL}/auth/mediaRefs?${queryString}`, {
-    method: 'get',
-    headers: {
-      Authorization: bearerToken,
-      nsfwMode
-    },
-    withCredentials: true
-  })
-}
-
-export const getLoggedInUserPlaylists = async (bearerToken, page = 1) => {
-  let filteredQuery: any = {}
-  filteredQuery.page = page
-  const queryString = convertObjectToQueryString(filteredQuery)
-
-  return axios(`${API_BASE_URL}/auth/playlists?${queryString}`, {
-    method: 'get',
-    data: {
-      page
-    },
-    headers: {
-      Authorization: bearerToken
-    },
     withCredentials: true
   })
 }
@@ -97,46 +62,6 @@ export const signUp = async (email: string, password: string) => {
       email,
       password
     },
-    withCredentials: true
-  })
-}
-
-export const deleteLoggedInUser = async (id: string) => {
-  return axios(`${API_BASE_URL}/auth/user`, {
-    method: 'delete',
-    withCredentials: true
-  })
-}
-
-export const updateLoggedInUser = async (data: any) => {
-  return axios(`${API_BASE_URL}/auth/user`, {
-    method: 'patch',
-    data,
-    withCredentials: true
-  })
-}
-
-export const addOrUpdateUserHistoryItem = async (nowPlayingItem: NowPlayingItem) => {
-  return axios(`${API_BASE_URL}/auth/user/add-or-update-history-item`, {
-    method: 'patch',
-    data: {
-      historyItem: nowPlayingItem
-    },
-    withCredentials: true
-  })
-}
-
-export const downloadLoggedInUserData = async (id: string) => {
-  return axios(`${API_BASE_URL}/auth/user/download`, {
-    method: 'get',
-    withCredentials: true
-  })
-}
-
-export const updateUserQueueItems = async (data: any) => {
-  return axios(`${API_BASE_URL}/auth/user/update-queue`, {
-    method: 'patch',
-    data,
     withCredentials: true
   })
 }
--- src/services/user.ts
+++ src/services/user.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { sfwFilterMediaRefs, sfwFilterPlaylists, sfwFilterUser, sfwFilterUsers
   } from '~/lib/profanityFilter'
+import { NowPlayingItem } from '~/lib/nowPlayingItem'
 import { convertObjectToQueryString } from '~/lib/utility'
 import config from '~/config'
 const { API_BASE_URL } = config()
@@ -78,6 +79,81 @@ export const getPublicUsersByQuery = async (query, nsfwMode = 'on') => {
 export const toggleSubscribeToUser = async (userId: string) => {
   return axios(`${API_BASE_URL}/user/toggle-subscribe/${userId}`, {
     method: 'get',
+    withCredentials: true
+  })
+}
+
+
+export const getLoggedInUserMediaRefs = async (bearerToken, nsfwMode, sort = 'most-recent', page = 1) => {
+  let filteredQuery: any = {}
+  filteredQuery.sort = sort
+  filteredQuery.page = page
+  const queryString = convertObjectToQueryString(filteredQuery)
+
+  return axios(`${API_BASE_URL}/mediaRefs?${queryString}`, {
+    method: 'get',
+    headers: {
+      Authorization: bearerToken,
+      nsfwMode
+    },
+    withCredentials: true
+  })
+}
+
+export const getLoggedInUserPlaylists = async (bearerToken, page = 1) => {
+  let filteredQuery: any = {}
+  filteredQuery.page = page
+  const queryString = convertObjectToQueryString(filteredQuery)
+
+  return axios(`${API_BASE_URL}/playlists?${queryString}`, {
+    method: 'get',
+    data: {
+      page
+    },
+    headers: {
+      Authorization: bearerToken
+    },
+    withCredentials: true
+  })
+}
+
+
+export const deleteLoggedInUser = async (id: string) => {
+  return axios(`${API_BASE_URL}/user`, {
+    method: 'delete',
+    withCredentials: true
+  })
+}
+
+export const updateLoggedInUser = async (data: any) => {
+  return axios(`${API_BASE_URL}/user`, {
+    method: 'patch',
+    data,
+    withCredentials: true
+  })
+}
+
+export const addOrUpdateUserHistoryItem = async (nowPlayingItem: NowPlayingItem) => {
+  return axios(`${API_BASE_URL}/user/add-or-update-history-item`, {
+    method: 'patch',
+    data: {
+      historyItem: nowPlayingItem
+    },
+    withCredentials: true
+  })
+}
+
+export const downloadLoggedInUserData = async (id: string) => {
+  return axios(`${API_BASE_URL}/user/download`, {
+    method: 'get',
+    withCredentials: true
+  })
+}
+
+export const updateUserQueueItems = async (data: any) => {
+  return axios(`${API_BASE_URL}/user/update-queue`, {
+    method: 'patch',
+    data,
     withCredentials: true
   })
 }
