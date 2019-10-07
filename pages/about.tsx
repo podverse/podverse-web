e9592de16564fd9c7aa2ed26a810c51fee32e0e5
Mitch Downey <mitchldowney@gmail.com>
2019-10-07 18:29:19 -0500

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -53,31 +53,29 @@ class About extends Component<Props, State> {
 
         <h3>About</h3>
         <p>
-          Podverse makes it easy to create, share, and discover full-length clips from your favorite podcasts.
-          Other features include an intuitive streamlined design, sharable playlists, user profiles,
-          and auto-syncing your queue and subscriptions across all your devices.
+          <span class='font-bold'>Podverse makes it easy to create, share, and discover full-length clips from your favorite podcasts. </span>
+          Additional features include an intuitive streamlined design, sharable playlists, user profiles,
+          and auto-syncing your queue and subscriptions across all of your devices.
         </p>
 
         <p>
           Our mission is to create the most world's most artist and user-friendly podcast app.
-          All Podverse software is provided under an open source <i>copyleft</i> license. Anyone can freely download
+          All Podverse software is provided under an open source copyleft license. Anyone can freely download
           Podverse software and use it for any purpose, as long as they also share their changes to the code.
-          Furthermore, we are committed to following humane technology principles to show the highest level
-          of respect for your time and data.
         </p>
 
         <p>
-          This project has been a labor of love for over 5 years now, and we hope to continue on it for the rest
-          of our lives. Our goal is to be a force for good in the podcasting industry,
-          defending the independent spirit of podcasting from monopolies and centralization,
-          while empowering artists with equal access to open source technology.
+          Furthermore, we are committed to following humane technology principles to respect your time, attention, and data.
         </p>
 
         <p>
-          If you have any questions, please reach out to us by emailing
-          <a href='mailto:contact@podverse.fm'>contact@podverse.fm</a>.
-          Thanks for checking out the site!
+          If you have any questions or feedback please reach out by emailing&nbsp;<a href='mailto:contact@podverse.fm'>contact@podverse.fm</a>.
+          Thank you!
         </p>
+
+        <p><a href='https://apps.apple.com/us/app/podverse/id1390888454'>Podverse on the App Store</a></p>
+
+        <p><a href='https://play.google.com/store/apps/details?id=com.podverse&hl=en_US'>Podverse on the Play Store</a></p>
 
         <hr />
 
@@ -85,9 +83,7 @@ class About extends Component<Props, State> {
         <p>Never sell or share private user data.</p>
         <p>Never add advertisements without podcaster permission.</p>
         <p>Allow users to download their complete data, so they can leave the site at any time.</p>
-        <p>Strive to make all our software meet ethical technology standards, like those laid out by the
-          <a href='https://humanetech.com/' target='_blank'>Center for Humane Technology</a>.
-        </p>
+        <p>Strive to make all Podverse software meet ethical technology standards, such as those provided by the&nbsp;<a href='https://humanetech.com/' target='_blank'>Center for Humane Technology</a>.</p>
 
         <hr />
 
