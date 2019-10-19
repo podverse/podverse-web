832467678e9bf740353bb7c30dcd5de2cfc531ba
Mitch Downey <mitchldowney@gmail.com>
2019-10-19 18:14:34 -0500

    Enable apple-app-site-association

--- src/server/routes/main.ts
+++ src/server/routes/main.ts
@@ -51,11 +51,11 @@ export default (app) => {
     await app.render(ctx.req, ctx.res, '/profiles', ctx.query)
   })
 
-  // router.get('/apple-app-site-association', async ctx => {
-  //   const aasa = fs.readFileSync(path.resolve(__dirname, '../../config/apple-app-site-association'))
-  //   ctx.set('Content-Type', 'application/json')
-  //   ctx.body = aasa
-  // })
+  router.get('/apple-app-site-association', async ctx => {
+    const aasa = fs.readFileSync(path.resolve(__dirname, '../../config/apple-app-site-association'))
+    ctx.set('Content-Type', 'application/json')
+    ctx.body = aasa
+  })
 
   return router
 }
