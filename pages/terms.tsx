e0b743a278076ba38f3ff33cbe037449886309e9
Mitch Downey <mitchldowney@gmail.com>
2020-01-03 00:01:48 -0600

    Update terms.tsx

--- pages/terms.tsx
+++ pages/terms.tsx
@@ -50,13 +50,16 @@ class Terms extends Component<Props, State> {
           All audio files found on podverse.fm load from the podcaster's own public server. We do not host podcast audio files ourselves.
         </p>
         <p>
-          All clips hosted on podverse.fm are crowd-sourced and unofficial, unless otherwise noted by the podcaster.
+          All clips hosted on podverse.fm are crowd-sourced and unofficial, unless otherwise noted by the podcaster themselves.
         </p>
         <p>
           Clips load within the full episode's media file, so the user always has access to the full recording.
         </p>
         <p>
           We host podcast links and content from third-party podcast feeds and sites. These have their own independent privacy policies, and we have no responsibility for their content or activities.
+        </p>
+        <p>
+          We also host reduced size podcast images from podcasters' feeds, to save user network data and speed up page loading times.
         </p>
       </Fragment>
     )
