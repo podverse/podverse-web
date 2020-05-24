52ec31904de7deeec62f1caee65fe7ad0e958cbe
Mitch Downey <mitchldowney@gmail.com>
2020-05-24 15:21:37 -0500

    Update Meta.tsx

--- src/components/Meta/Meta.tsx
+++ src/components/Meta/Meta.tsx
@@ -5,6 +5,7 @@ import '~/scss/styles.scss'
 import config from '~/config'
 const { metaDefaultImageUrl1200x630 } = config()
 const striptags = require('striptags')
+const uuidv4 = require('uuid/v4')
 
 type Props = {
   description?: string
@@ -32,8 +33,12 @@ class Meta extends Component<Props, State> {
       ogTitle = '', ogType = '', ogUrl = '', robotsNoIndex = '', title = '', twitterDescription = '', twitterImage = '',
       twitterImageAlt = '', twitterTitle = '' } = this.props
     
+    // Twitter images are cached by URL, and do not update. To make sure the latest image is used,
+    // we're setting the cacheBustDateTime as a url parameter in the meta tag.
+    const cacheBustDateTime = '-' + new Date().toISOString().slice(0, 10) + '-' + new Date().getHours()
+
     const ogImg = !ogImage ? metaDefaultImageUrl1200x630 : ogImage
-    const twitterImg = !twitterImage ? metaDefaultImageUrl1200x630 : twitterImage
+    const twitterImg = !twitterImage ? metaDefaultImageUrl1200x630 : twitterImage + cacheBustDateTime
 
     const strippedDescription = striptags(description)
     const strippedOgDescription = striptags(ogDescription)
