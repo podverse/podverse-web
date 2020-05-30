1c854e50c0f7c7dd385b9914461f6cb499004c0e
Mitch Downey <mitchldowney@gmail.com>
2020-05-29 22:43:19 -0500

    Add _resetDatabase helper for tests

--- __tests__/e2e/extensions/resetDatabase.js
+++ __tests__/e2e/extensions/resetDatabase.js
@@ -1,6 +1,7 @@
 const Client = require('ssh2').Client
+const path = require('path')
 
-const resetDatabase = () => {
+module.exports.command = function () {
   const resetDB = `docker stop podverse_db_stage; docker rm podverse_db_stage; docker-compose -f ./podverse-ops/docker-compose.stage.yml up -d podverse_db; sleep 10; PGPASSWORD='${process.env.STAGE_DATABASE_PASSWORD}' psql -h localhost -U postgres -d postgres -f ./podverse-ops/sample-database/qa-database.sql;`
   const conn = new Client()
   conn
@@ -30,10 +31,10 @@ const resetDatabase = () => {
       host: process.env.STAGE_SSH_HOST,
       port: process.env.STAGE_SSH_PORT,
       username: process.env.STAGE_SSH_USERNAME,
-      privateKey: require('fs').readFileSync('../../../stage_ssh_key')
+      privateKey: require('fs').readFileSync(path.resolve(__dirname, '../../../stage_ssh_key'))
     })
-}
 
-module.exports = {
-  resetDatabase
+  this.pause(30000)
+
+  return this
 }
