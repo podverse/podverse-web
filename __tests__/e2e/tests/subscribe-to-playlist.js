81e5d40af191493b8c8d1438d4a8748301ca0fc4
KyleFDowney <31670210+KyleFDowney@users.noreply.github.com>
2020-06-08 19:06:16 -0500

    Added tests for subscribing to a Playlist, Podcast, and Profile

---
+++ __tests__/e2e/tests/subscribe-to-playlist.js
@@ -0,0 +1,32 @@
+const { WEB_ORIGIN } = require('../constants')
+
+module.exports = {
+  before: function (browser) {
+      browser
+        .url(`${WEB_ORIGIN}/`)
+
+  },
+  'Subscribe to Playlist': function (browser) {
+    
+      browser
+        .loginUsingModal('premium@stage.podverse.fm')
+        .waitForXpathPresent(`//div[contains(text(), "Amet aliquam id diam maecenas ultricies mi eget.")]`)
+
+        .url(`${WEB_ORIGIN}/playlist/wgOok7Xp`)
+
+      browser.expect.element(`.media-header__subscribe svg[data-prefix="fas"]`).to.be.present
+      browser.click(`.media-header__subscribe`)
+      browser.refresh()
+      browser.expect.element(`.media-header__subscribe svg[data-prefix="far"]`).to.be.present
+      browser.click(`.media-header__subscribe`)
+      browser.refresh()
+      browser.expect.element(`.media-header__subscribe svg[data-prefix="fas"]`).to.be.present
+        
+
+
+
+  },
+  after: function (browser) {
+      browser.end()
+  }
+}
