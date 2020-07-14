bfb3c4980cd187733ad783008a1eb22513af5fcd
Mitch Downey <mitchldowney@gmail.com>
2020-07-13 22:26:43 -0500

    Disable pay with BitPay button

--- src/components/CheckoutModal/CheckoutModal.tsx
+++ src/components/CheckoutModal/CheckoutModal.tsx
@@ -110,7 +110,7 @@ class CheckoutModal extends React.Component<Props, State> {
             hideCheckoutModal={this.handleHideModal}
             updateStateAfterScriptLoads={this.handleButtonIsNotLoading}
             total={10} />
-          {
+          {/* {
             !buttonIsLoading &&
               <input 
                 alt="Pay with BitPay"
@@ -119,7 +119,7 @@ class CheckoutModal extends React.Component<Props, State> {
                 onClick={this.createBitPayOrder}
                 src="/images/bitpay-btn-pay.svg"
                 type="image" />
-          }
+          } */}
         </div>
       </Modal>
     )
