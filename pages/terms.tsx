50df42f0f62d48626d5b9aee89b0463c6f170402
Kyle Downey <capntechbeard@gmail.com>
2020-07-22 13:51:16 -0500

    Strings>Constants Cont.

--- pages/terms.tsx
+++ pages/terms.tsx
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import Meta from '~/components/Meta/Meta'
 import config from '~/config'
+import PV from '~/lib/constants'
 import { pageIsLoading } from '~/redux/actions'
 const { BASE_URL } = config()
 
@@ -17,9 +18,9 @@ class Terms extends Component<Props, State> {
     store.dispatch(pageIsLoading(false))
 
     const meta = {
-      currentUrl: BASE_URL + '/terms',
-      description: `Podverse terms of service agreement.`,
-      title: `Podverse - Terms of Service`
+      currentUrl: BASE_URL + PV.paths.terms,
+      description: PV.pages.terms._Description,
+      title: PV.pages.terms._Title
     }
 
     return { meta }
@@ -40,27 +41,27 @@ class Terms extends Component<Props, State> {
           title={meta.title}
           twitterDescription={meta.description}
           twitterTitle={meta.title} />
-        <h3>Terms of Service</h3>
+        <h3>{PV.pages.terms.TermsOfService}</h3>
         <p>
-          Podverse will never sell or share private user data.
+          {PV.pages.terms.PodverseTerms_1}
         </p>
         <p>
-          We will never put advertisements next to or within a podcast's content without that podcast's permission.
+          {PV.pages.terms.PodverseTerms_2}
         </p>
         <p>
-          All audio files found on podverse.fm load from the podcaster's own public server. We do not host podcast audio files ourselves.
+          {PV.pages.terms.PodverseTerms_3}
         </p>
         <p>
-          All clips hosted on podverse.fm are crowd-sourced and unofficial, unless otherwise noted by the podcaster themselves.
+          {PV.pages.terms.PodverseTerms_4}
         </p>
         <p>
-          Clips load within the full episode's media file, so the user always has access to the full recording.
+          {PV.pages.terms.PodverseTerms_5}
         </p>
         <p>
-          We host podcast links and content from third-party podcast feeds and sites. These have their own independent privacy policies, and we have no responsibility for their content or activities.
+          {PV.pages.terms.PodverseTerms_6}
         </p>
         <p>
-          We also host reduced size podcast images from podcasters' feeds, to save user network data and speed up page loading times.
+          {PV.pages.terms.PodverseTerms_7}
         </p>
       </Fragment>
     )
