5561411fe0c1692e8ffd9786ee21b3d83e0a0a46
Mitch Downey <mitchldowney@gmail.com>
2020-09-25 20:54:08 -0500

    Update _resetDatabase.js

--- __tests__/e2e/tests/_resetDatabase.js
+++ __tests__/e2e/tests/_resetDatabase.js
@@ -4,11 +4,12 @@ module.exports = {
   before: function (browser) {
     browser
       ._resetDatabase()
+      .pause(120000)
       .url(`${WEB_ORIGIN}/`)
   },
   'Reset Database': function (browser) {
     browser
-      .waitForElementWithText('h3', 'Clips', 300000)
+      .waitForElementWithText('h3', 'Clips')
   },
   after: function (browser) {
     browser.end()
