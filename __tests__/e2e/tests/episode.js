ab23b2ff0887d555441f1ffaad12f301ff8a0b54
Mitch Downey <mitchldowney@gmail.com>
2020-09-25 22:52:03 -0500

    Update episode.js

--- __tests__/e2e/tests/episode.js
+++ __tests__/e2e/tests/episode.js
@@ -15,14 +15,14 @@ module.exports = {
       .waitForXpathPresent('//div[contains(text(), "The Americans are coming, but will the war be over by the time they get there? Germany throws everything into a last series of stupendous attacks in the West while hoping to avoid getting burned by a fire in the East they helped fan.")]')
       .testSharedMetaTags()
       .testPageMetaTags(
-        `Show 55 - Blueprint for Armageddon VI - Dan Carlin's Hardcor`,
-        `The Americans are coming, but will the war be over by the time they get there Germany throws everything into a last series of stupendous attacks in the West whi`
+        `Show 55 - Blueprint for Armageddon VI - Dan Carlin's Hardcore History`,
+        `The Americans are coming, but will the war be over by the time they get there Germany throws everything into a last series of stupendous attacks in the West while hoping to avoid getting burned by a fire in the East they helped fan.`
       )
       .checkCurrentMedia(`Show 55 - Blueprint for Armageddon VI`, `episode`)
       .scrollToSelector(mediaListSelectsSelector)
       .click('xpath', dropdownToggleClipsXpath)
       .click('xpath', dropdownItemEpisodesXpath)
-      .waitForXpathPresent('//div[contains(text(), "Show 55 - Blueprint for Armageddon VI")]')
+      .waitForXpathPresent(`//div[contains(text(), "Show 55 - Blueprint for Armageddon VI")]`)
       .click('xpath', dropdownToggleEpisodesXpath)
       .click('xpath', dropdownItemClipsXpath)
       .waitForXpathPresent('//div[contains(text(), "Ornare aenean euismod elementum nisi quis eleifend quam adipiscing vitae.")]')
