9c57b3ca7c89568bfdb36237a2ad731c0a362c93
Mitch Downey <mitchldowney@gmail.com>
2020-12-12 21:31:25 -0600

    Update index.ts

--- src/lib/utility/index.ts
+++ src/lib/utility/index.ts
@@ -203,7 +203,7 @@ export const alertSomethingWentWrong = (t) => {
 export const removeDoubleQuotes = str => str ? str.replace(/["]+/g, '') : ''
 
 // Remove nonAlphanumeric characters that are not supported by search crawlers
-const seoRemoveNonAlphanumericCharacters = str => str.replace(/[^0-9a-z-_–:,'.%\s]/gi, '')
+const seoRemoveNonAlphanumericCharacters = str => str.replace(/[^0-9a-z-_–:,'’.%\s]/gi, '')
 
 // Titles are limited to ~60 characters by Google
 export const seoMetaTitle = str => {
