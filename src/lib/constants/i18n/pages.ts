bbbd1da9b90fa39c1dc3d8d983704c7d9b4d51c6
Mitch Downey <mitchldowney@gmail.com>
2021-07-24 23:00:33 -0500

    Add Popularity Analytics to the Terms of Service page

--- src/lib/constants/i18n/pages.ts
+++ src/lib/constants/i18n/pages.ts
@@ -9,6 +9,13 @@ export const pages = {
     PodverseTerms_5: `Clips load within the full episode's media file, so the user always has access to the full recording.`,
     PodverseTerms_6: `We host podcast links and content from third-party podcast feeds and sites. These have their own independent privacy policies, and we have no responsibility for their content or activities.`,
     PodverseTerms_7: `We also host reduced size podcast images from podcasters' feeds, to save user network data and speed up page loading times.`,
+    PopularityAnalyticsHeader: `Popularity Analytics`,
+    PopularityAnalytics_1: `Podverse enables anonymized, cookie-less tracking by default on this website. (To disable this tracking, please use a tracker blocking web browser.)`,
+    PopularityAnalytics_2: `We use this tracking information to help determine which content on the site (including podcasts, episodes, and clips) are most popular.`,
+    PopularityAnalytics_3: `The tracking software we use is called Matomo, and it is a free and open source analytics platform. We host an instance of Matomo, so all tracking data is hosted on our servers, and individual user tracking data will never be sold to or shared with 3rd parties.`,
+    PopularityAnalytics_4: `All of our tracking data is anonymized by obscuring the IP address by 2 bytes, which means IP addresses are obscured and then saved on our server with the format 192.168.xxx.xxx.`,
+    PopularityAnalytics_5: `To further protect user anonymity, we do not use session IDs to track users across multiple visits. Likewise, to further protect anonymity, we do not use cookie-based tracking on our website.`,
+    PopularityAnalytics_6: `While we use Matomo tracking data to help us determine what content is most popular on Podverse, users should keep in mind that these popularity metrics are easy to manipulate, and may not accurately reflect the popularity of content.`,
     TermsOfService: `Terms of Service`
   }
 }
