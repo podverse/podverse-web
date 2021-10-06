57db2cf90f3eda7fecae5ffd1b498b4b5260e52a
Mitch Downey <mitchldowney@gmail.com>
2021-10-06 17:53:02 -0500

    #257 Support ? and ! in meta tag titles

--- src/lib/utility/index.ts
+++ src/lib/utility/index.ts
@@ -207,7 +207,7 @@ export const alertSomethingWentWrong = (t) => {
 }
 
 // Remove nonAlphanumeric characters that are not supported by search crawlers
-const seoRemoveNonAlphanumericCharacters = str => str.replace(/[^0-9a-z-_–:,'’.%\s]/gi, '')
+const seoRemoveNonAlphanumericCharacters = str => str.replace(/[^0-9a-z-_–:,'’"?!.%\s]/gi, '')
 
 // Titles are limited to ~60 characters by Google
 export const seoMetaTitle = str => {
