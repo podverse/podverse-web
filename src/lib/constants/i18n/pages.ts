5df5f8d40ae18056357ce3c702ddcb1066b70ed0
Mitch Downey <mitchldowney@gmail.com>
2021-10-24 19:31:26 -0500

    Add video to terms and conditions

--- src/lib/constants/i18n/pages.ts
+++ src/lib/constants/i18n/pages.ts
@@ -4,7 +4,7 @@ export const pages = {
   terms: {
     PodverseTerms_1: `Podverse will never sell or share private user data.`,
     PodverseTerms_2: `We will never put advertisements next to or within a podcast's content without that podcast's permission.`,
-    PodverseTerms_3: `All audio files found on podverse.fm load from the podcaster's own public server. We do not host podcast audio files ourselves.`,
+    PodverseTerms_3: `All audio or video files found on podverse.fm load from the podcaster's own public server. We do not host podcast audio or video files ourselves.`,
     PodverseTerms_4: `All clips hosted on podverse.fm are crowd-sourced and unofficial, unless otherwise noted by the podcaster themselves.`,
     PodverseTerms_5: `Clips load within the full episode's media file, so the user always has access to the full recording.`,
     PodverseTerms_6: `We host podcast links and content from third-party podcast feeds and sites. These have their own independent privacy policies, and we have no responsibility for their content or activities.`,
