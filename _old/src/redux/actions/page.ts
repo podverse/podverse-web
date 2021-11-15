e1f094d349d263c241e048b8e8561c55576c644a
Mitch Downey <mitchldowney@gmail.com>
2021-11-15 17:10:24 -0600

    Remove old app code

--- _old/src/redux/actions/page.ts
+++
@@ -1,8 +0,0 @@
-import { actionTypes } from '~/redux/constants'
-
-export const pageIsLoading = payload => {
-  return {
-    type: actionTypes.PAGE_IS_LOADING,
-    payload
-  }
-}
--- _old/src/redux/store.ts
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import thunkMiddleware from 'redux-thunk'
-import reducers from '~/redux/reducers'
-
-export const initializeStore = (initialState = {}) => {
-  return createStore(reducers, initialState, composeWithDevTools(applyMiddleware(thunkMiddleware)))
-}
