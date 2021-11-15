a27039a51540472a6adb4eb385cbf7158da5ec7e
Raj Sheth <rajpsheth@gmail.com>
2021-11-16 00:23:36 +0530

    Add Spinner in Button Component

--- src/components/Buttons/ButtonRectangle.tsx
+++ src/components/Buttons/ButtonRectangle.tsx
@@ -5,22 +5,28 @@ type Props = {
   label: string
   onClick?: any
   type: 'primary' | 'secondary' | 'tertiary'
+  isLoading?: boolean
 }
 
-export const ButtonRectangle = ({ disabled, label, onClick, type }: Props) => {
+export const ButtonRectangle = ({ disabled, label, onClick, type, isLoading }: Props) => {
   const buttonClass = classnames(
     'button-rectangle',
     type === 'primary' ? 'primary' : '',
     type === 'secondary' ? 'secondary' : '',
-    type === 'tertiary' ? 'tertiary' : ''
+    type === 'tertiary' ? 'tertiary' : '',
+    isLoading ? 'loading' : '',
   )
 
   return (
-    <button
-      className={buttonClass}
-      disabled={disabled}
-      onClick={onClick}>
-      {label}
-    </button>
+    <div>
+      <button
+        className={buttonClass}
+        disabled={disabled}
+        onClick={onClick}>
+        <span className="button__text">
+          {label}
+        </span>
+      </button>
+    </div>
   )
 }
