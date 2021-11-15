0d7c93e625bb371700c37178efcceb8ef9abc9fc
Creon Creonopoulos <kreonjr@hotmail.com>
2021-11-15 16:42:48 -0600

    Player info component start

--- src/components/Player/PlayerItemInfo.tsx
+++ src/components/Player/PlayerItemInfo.tsx
@@ -1,13 +1,23 @@
 import classnames from "classnames"
 
 type Props = {
+  imgSource: string | null,
+  podcastTitle: string | null,
+  mediaTitle: string | null
 }
 
 export const PlayerItemInfo = ({}: Props) => {
 
+  const container = classnames(
+    "item-info-container"
+)
+
   return (
-    <div>
-      {"Info"}
+    <div className={container}>
+      <img/>
+      <div>
+
+      </div>
     </div>
   )
 }
