8180424aba5f9ed64a103ab80897e46c5d9b4158
KyleFDowney <capntechbeard@gmail.com>
2021-11-19 15:50:59 -0600

    cont. web tests

--- __tests__/e2e/tests/login.js
+++ __tests__/e2e/tests/login.js
@@ -8,7 +8,29 @@ module.exports = {
     browser
       .click('.react-dropdown-select')
       .click('.react-dropdown-select-item:nth-child(3)')
-      .waitForElementWithText('.ReactModal__Content h2', 'Login')
+      .waitForElementWithText('.ReactModal__Content h2', 'Log in')
+      .waitForXpathPresent(`//button[@class="button-close"]`)
+      .click('.button-close')      
+      .waitForXpathNotPresent(`//button[@class="button-close"]`)
+
+      .click('.react-dropdown-select')
+      .click('.react-dropdown-select-item:nth-child(3)')
+      .waitForElementWithText('.ReactModal__Content h2', 'Log in')
+      .click('.button-rectangle.secondary')      
+
+      .click('.react-dropdown-select')
+      .click('.react-dropdown-select-item:nth-child(3)')
+      .waitForElementWithText('button.button-link:nth-child(1) ', 'Reset Password')
+      .click('button.button-link:nth-child(1)')
+      .waitForElementWithText('.ReactModal__Content h2', 'Forgot Password')
+      .click('.button-close')
+
+      .click('.react-dropdown-select')
+      .click('.react-dropdown-select-item:nth-child(3)')
+      .waitForElementWithText('button.button-link:nth-child(2) ', 'Sign Up')
+      .click('button.button-link:nth-child(2)')
+      .waitForElementWithText('.ReactModal__Content h2', 'Sign Up')
+      .click('.button-close')
       
 
 
