78835036157f6a77cf4967429763ad8e162e1b81
Mitch Downey <mitchldowney@gmail.com>
2021-11-21 13:45:50 -0600

    Try to fix Docker image stage env var handling

--- next.config.js
+++ next.config.js
@@ -2,5 +2,11 @@ const { i18n } = require('./next-i18next.config')
 
 module.exports = {
   reactStrictMode: true,
-  i18n
+  i18n,
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_API_PROTOCOL: process.env.NEXT_PUBLIC_API_PROTOCOL,
+    NEXT_PUBLIC_API_DOMAIN: process.env.NEXT_PUBLIC_API_DOMAIN,
+    NEXT_PUBLIC_API_PATH: process.env.NEXT_PUBLIC_API_PATH,
+    NEXT_PUBLIC_API_VERSION: process.env.NEXT_PUBLIC_API_VERSION
+  }
 }
--- src/resources/Config.ts
+++ src/resources/Config.ts
@@ -1,16 +1,10 @@
-/*
-  TODO: env vars are being hardcoded because of an issue where env vars
-  are not getting passed to the client-side in Docker containers :(
-  
-  Any help implementing a good solution for this so we don't have to
-  hardcode values and can use a .env instead would be greatly appreciated.
+import getConfig from 'next/config'
+const { publicRuntimeConfig } = getConfig()
 
-  More info: https://github.com/vercel/next.js/discussions/17641
-*/
-const API_PROTOCOL = 'http'
-const API_DOMAIN = 'localhost:1234'
-const API_PATH = 'api'
-const API_VERSION = 'v1'
+const API_PROTOCOL = publicRuntimeConfig.NEXT_PUBLIC_API_PROTOCOL
+const API_DOMAIN = publicRuntimeConfig.NEXT_PUBLIC_API_DOMAIN
+const API_PATH = publicRuntimeConfig.NEXT_PUBLIC_API_PATH
+const API_VERSION = publicRuntimeConfig.NEXT_PUBLIC_API_VERSION
 const API_BASE_URL = `${API_PROTOCOL}://${API_DOMAIN}/${API_PATH}/${API_VERSION}`
 
 export const Config = {
