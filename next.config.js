e71553d57d356743b36cc64908765215fed326e1
Mitch Downey <mitchldowney@gmail.com>
2021-11-21 14:38:24 -0600

    Continue trying to fix prod env vars

--- next.config.js
+++ next.config.js
@@ -1,11 +1,21 @@
 const { i18n } = require('./next-i18next.config')
 
+const envVars = {
+  
+}
+
 module.exports = {
   reactStrictMode: true,
   i18n,
-  publicRuntimeConfig: {
-    API_PROTOCOL: process.env.API_PROTOCOL,
+  serverRuntimeConfig: {
     API_DOMAIN: process.env.API_DOMAIN,
+    API_PROTOCOL: process.env.API_PROTOCOL,
+    API_PATH: process.env.API_PATH,
+    API_VERSION: process.env.API_VERSION
+  },
+  publicRuntimeConfig: {
+    API_DOMAIN: process.env.PUBLIC_API_DOMAIN,
+    API_PROTOCOL: process.env.PUBLIC_API_PROTOCOL,
     API_PATH: process.env.API_PATH,
     API_VERSION: process.env.API_VERSION
   }
