8817ceda2fa2c90f08daa58bd38e09a26702bcba
Mitch Downey <mitchldowney@gmail.com>
2021-11-21 14:20:39 -0600

    Try to fix Docker hostname bug 2

--- src/resources/Config.ts
+++ src/resources/Config.ts
@@ -1,10 +1,10 @@
 import getConfig from 'next/config'
 const { publicRuntimeConfig } = getConfig()
 
-const API_PROTOCOL = publicRuntimeConfig.NEXT_PUBLIC_API_PROTOCOL
-const API_DOMAIN = publicRuntimeConfig.NEXT_PUBLIC_API_DOMAIN
-const API_PATH = publicRuntimeConfig.NEXT_PUBLIC_API_PATH
-const API_VERSION = publicRuntimeConfig.NEXT_PUBLIC_API_VERSION
+const API_PROTOCOL = publicRuntimeConfig.API_PROTOCOL
+const API_DOMAIN = publicRuntimeConfig.API_DOMAIN
+const API_PATH = publicRuntimeConfig.API_PATH
+const API_VERSION = publicRuntimeConfig.API_VERSION
 const API_BASE_URL = `${API_PROTOCOL}://${API_DOMAIN}/${API_PATH}/${API_VERSION}`
 
 export const Config = {
