868282b26eb13ce20932cf33ddfd34b6a53edb2b
Mitch Downey <mitchldowney@gmail.com>
2021-11-27 17:53:31 -0600

    Make PlayerItemInfo show full view onClick

--- src/components/Player/PlayerItemInfo.tsx
+++ src/components/Player/PlayerItemInfo.tsx
@@ -1,4 +1,5 @@
 import classnames from "classnames"
+import OmniAural from 'omniaural'
 import { PVImage } from "~/components"
 import type { NowPlayingItem } from "podverse-shared"
 import { PV } from "~/resources"
@@ -11,21 +12,27 @@ type Props = {
 export const PlayerItemInfo = ({ nowPlayingItem }: Props) => {
   const { t } = useTranslation()
   const container = classnames("player-item-info-container")
+  const contentsContainer = classnames("player-item-contents")
   const textContainer = classnames("player-item-info-text-container")
   const title = classnames("item-info-title")
   const subtitle = classnames("item-info-subtitle")
 
   return (
     <div className={container}>
-      <PVImage
-        alt={t("Podcast artwork")}
-        height={PV.Images.sizes.medium}
-        width={PV.Images.sizes.medium}
-        src={nowPlayingItem.episodeImageUrl || nowPlayingItem.podcastImageUrl}
-      />
-      <div className={textContainer}>
-        <div className={title}>{nowPlayingItem.clipTitle || nowPlayingItem.episodeTitle}</div>
-        <div className={subtitle}>{nowPlayingItem.podcastTitle}</div>
+      <div
+        className={contentsContainer}
+        onClick={OmniAural.playerFullViewShow}
+        tabIndex={0}>
+        <PVImage
+          alt={t("Podcast artwork")}
+          height={PV.Images.sizes.medium}
+          width={PV.Images.sizes.medium}
+          src={nowPlayingItem.episodeImageUrl || nowPlayingItem.podcastImageUrl}
+        />
+        <div className={textContainer}>
+          <div className={title}>{nowPlayingItem.clipTitle || nowPlayingItem.episodeTitle}</div>
+          <div className={subtitle}>{nowPlayingItem.podcastTitle}</div>
+        </div>
       </div>
     </div>
   )
