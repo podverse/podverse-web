de484dea6656c4c48d11621a9c01dc2e82a33b47
Mitch Downey <mitchldowney@gmail.com>
2021-12-03 16:17:00 -0600

    Run prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -39,10 +39,14 @@ if (typeof window !== 'undefined') {
 }
 
 function MyApp({ Component, pageProps }) {
-  const { serverHistoryItemsIndex = {
-    "episodes": {},
-    "mediaRefs": {}
-  }, serverUserInfo = null, serverUserQueueItems = [] } = pageProps
+  const {
+    serverHistoryItemsIndex = {
+      episodes: {},
+      mediaRefs: {}
+    },
+    serverUserInfo = null,
+    serverUserQueueItems = []
+  } = pageProps
   OmniAural.setUserInfo(serverUserInfo)
   OmniAural.setUserQueueItems(serverUserQueueItems)
   OmniAural.setHistoryItemsIndex(serverHistoryItemsIndex)
