84a8e0de1e8120d5a1e5b9859dc245d981b0f241
Mitch Downey <mitchldowney@gmail.com>
2021-12-05 21:10:47 -0600

    Update List.tsx

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -9,16 +9,12 @@ type Props = {
 export const List = ({ children, noMarginTop }: Props) => {
   const { t } = useTranslation()
   const hasChildren = children && children[0] && children[0].length > 0
-  const listClass = classNames('list', noMarginTop && hasChildren ? 'no-margin-top' : '')  
+  const listClass = classNames('list', noMarginTop && hasChildren ? 'no-margin-top' : '')
 
   return (
     <ul className={listClass}>
-      {
-        !hasChildren && <div className='no-results-found'>{t('No results found')}</div>
-      }
-      {
-        hasChildren && children
-      }
+      {!hasChildren && <div className='no-results-found'>{t('No results found')}</div>}
+      {hasChildren && children}
     </ul>
   )
 }
