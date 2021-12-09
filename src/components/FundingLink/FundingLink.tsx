720fcbd9958ba4ecd93a93761f01f013305df82d
eurbasius <eur@whiteaway.com>
2021-12-09 06:48:32 +0100

    linting

--- src/components/FundingLink/FundingLink.tsx
+++ src/components/FundingLink/FundingLink.tsx
@@ -1,14 +1,14 @@
 type Props = {
-  link: string;
-  value?: string;
-};
+  link: string
+  value?: string
+}
 
 export const FundingLink = ({ link, value }: Props) => {
   return (
-    <div className="funding-link">
-      <a href={link} target="_blank">
+    <div className='funding-link'>
+      <a href={link} target='_blank'>
         {value}
       </a>
     </div>
-  );
-};
+  )
+}
