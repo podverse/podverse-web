32f334d393aba315ceaf804afea69d0d967619f8
Mitch Downey <mitchldowney@gmail.com>
2021-12-25 23:24:46 -0600

    Update V4VHiddenElement.tsx

--- src/components/V4VHiddenElement/V4VHiddenElement.tsx
+++ src/components/V4VHiddenElement/V4VHiddenElement.tsx
@@ -10,8 +10,8 @@ export const V4VHiddenElement = () => {
 
   let valueTag =
     currentNowPlayingItem.episodeValue?.length > 0
-    ? currentNowPlayingItem.episodeValue
-    : currentNowPlayingItem.podcastValue?.length > 0
+      ? currentNowPlayingItem.episodeValue
+      : currentNowPlayingItem.podcastValue?.length > 0
       ? currentNowPlayingItem.podcastValue
       : null
   valueTag = valueTag ? JSON.stringify(valueTag) : null
@@ -19,11 +19,12 @@ export const V4VHiddenElement = () => {
   return (
     <div
       className='v4v-hidden-element'
-      {...(paused ? {} : { "data-v4v-is-playing": true })}
+      {...(paused ? {} : { 'data-v4v-is-playing': true })}
       data-v4v-current-playback-position={playbackPosition}
       data-v4v-podcast-index-id={currentNowPlayingItem.podcastIndexPodcastId}
       data-v4v-podcast-title={currentNowPlayingItem.podcastTitle}
       data-v4v-episode-title={currentNowPlayingItem.episodeTitle}
-      data-v4v-value={valueTag} />
+      data-v4v-value={valueTag}
+    />
   )
 }
