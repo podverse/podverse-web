ad2e6c0d34e42980fbe3917aa969bf7c60fe81c9
Ryan Hirsch <ryan.hirsch@mixpanel.com>
2022-01-09 12:12:43 -0600

    fix: :green_heart: CI was broken due to f1a3fa86fd44d34d8f0ab21189533dbc59f4339a

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,31 +1,34 @@
-import Link from "next/link"
+import Link from 'next/link'
 
 export const Footer = () => {
-    return (
-      <footer className='footer'>
-          <div className="footer-middle">
-            <div className="footer-middle-site-links">
-                <ul>
-                    <li>
-                        <a href="/contact">Contact</a>
-                    </li>
-                    <li>
-                        <a href="/about">About</a>
-                    </li>
-                    <li>Terms</li>
-                    <li>Premium</li>
-                </ul>
-            </div>
-            <div className="footer-middle-site-links">
-                <ul>
-                    <li>GitHub</li>
-                    <li>Twitter</li>
-                    <li>Facebook</li>
-                    <li>Reddit</li>
-                </ul>
-            </div>
-          </div>
-      </footer>
-    )
-  }
-  
+  return (
+    <footer className='footer'>
+      <div className='footer-middle'>
+        <div className='footer-middle-site-links'>
+          <ul>
+            <li>
+              <Link href='/contact'>
+                <a>Contact</a>
+              </Link>
+            </li>
+            <li>
+              <Link href='/about'>
+                <a>About</a>
+              </Link>
+            </li>
+            <li>Terms</li>
+            <li>Premium</li>
+          </ul>
+        </div>
+        <div className='footer-middle-site-links'>
+          <ul>
+            <li>GitHub</li>
+            <li>Twitter</li>
+            <li>Facebook</li>
+            <li>Reddit</li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  )
+}
