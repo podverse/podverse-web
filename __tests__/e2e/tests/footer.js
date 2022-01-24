de7e52a593821723dea0ac97c79f279e162f9c4a
KyleFDowney <capntechbeard@gmail.com>
2022-01-24 15:55:10 -0600

    update footer test

--- __tests__/e2e/tests/footer.js
+++ __tests__/e2e/tests/footer.js
@@ -7,6 +7,39 @@ module.exports = {
   'Footer': function (browser) {
     browser
       .scrollToSelector('.footer-middle')
+      .waitForElement('.footer-social-link-mastodon')
+      .waitForElement('.footer-social-link-github')
+      .waitForElement('.footer-social-link-twitter')
+      .waitForElement('.footer-social-link-discord')
+
+
+      .waitForElement('.footer-link-contact')
+      .click('.footer-link-contact')
+      
+      .scrollToSelector('.footer-middle')
+      .waitForElement('.footer-link-about')
+      .click('.footer-link-about')
+
+      .scrollToSelector('.footer-middle')
+      .waitForElement('.footer-link-terms')
+      .click('.footer-link-terms')
+
+      .scrollToSelector('.footer-middle')
+      .waitForElement('.footer-link-premium')
+      .click('.footer-link-premium')
+
+      .scrollToSelector('.footer-middle')
+      .waitForElement('.footer-link-mobile-app')
+      .click('.footer-link-mobile-app')
+
+      .scrollToSelector('.footer-middle')
+      .waitForElement('.footer-link-support')
+      .click('.footer-link-support')
+
+
+
+
+      
 
   },
   after: function (browser) {
