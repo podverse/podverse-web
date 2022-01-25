5745a466d6e4e6570a6213aeeaa2597daca29452
KyleFDowney <capntechbeard@gmail.com>
2022-01-25 15:07:17 -0600

    update footer.js

--- __tests__/e2e/tests/footer.js
+++ __tests__/e2e/tests/footer.js
@@ -15,26 +15,32 @@ module.exports = {
 
       .waitForElement('.footer-link-contact')
       .click('.footer-link-contact')
+      .waitForElementWithText('.text-page h2', 'Email')
       
       .scrollToSelector('.footer-middle')
       .waitForElement('.footer-link-about')
       .click('.footer-link-about')
+      .waitForElementWithText('.text-page p.bigger', 'Podverse is an open source podcast manager for iOS, Android, and web.')
 
       .scrollToSelector('.footer-middle')
       .waitForElement('.footer-link-terms')
       .click('.footer-link-terms')
+      .waitForElementWithText('.text-page p', 'Podverse will never sell or share private user data.')
 
       .scrollToSelector('.footer-middle')
       .waitForElement('.footer-link-premium')
       .click('.footer-link-premium')
+      .waitForElementWithText('.text-page p', 'Get 3 months free when you sign up for Podverse premium')
 
       .scrollToSelector('.footer-middle')
       .waitForElement('.footer-link-mobile-app')
       .click('.footer-link-mobile-app')
+      .waitForElementWithText('.text-page p.bigger', 'Podverse is an open source podcast manager for iOS, Android, and web.')
 
       .scrollToSelector('.footer-middle')
       .waitForElement('.footer-link-support')
       .click('.footer-link-support')
+      .waitForElementWithText('.text-page p', 'Podverse creates free and open source software to expand what is possible in podcasting.')
 
 
 
