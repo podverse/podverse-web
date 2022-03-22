910c69ffa991228f347a9a4be88f1e6adc84cc51
Mitch Downey <mitchldowney@gmail.com>
2022-03-22 09:17:26 -0500

    Move threadcap helpers to podverse-shared

--- src/services/socialInteraction/threadcap.tsx
+++ src/services/socialInteraction/threadcap.tsx
@@ -1,14 +1,4 @@
-import {
-  ThreadcapAttachment,
-  ThreadcapNode,
-  ThreadcapResponse,
-  PVComment,
-  ThreadcapCommenter,
-  checkIfAllowedImageOrigin
-} from 'podverse-shared'
-import { removeUsernamesFromBeginningOfString } from 'podverse-shared/dist/socialInteraction/ActivityPub'
-import striptags from 'striptags'
-import { decodeHtml } from '~/lib/utility/misc'
+import { convertThreadcapResponseToPVComment } from 'podverse-shared'
 import { PV } from '~/resources'
 import { request } from '~/services/request'
 
@@ -34,79 +24,4 @@ export const getEpisodeProxyTwitter = async (episodeId: string) => {
   const comment = convertThreadcapResponseToPVComment(response.data)
   comment.isRoot = true
   return comment
-}
-
-const parseUserInfo = (comment: any, protocol: string, commenters: { [key: string]: ThreadcapCommenter }) => {
-  let username = ''
-  let profileIcon = ''
-  const commenter = commenters[comment.attributedTo]
-
-  if (protocol === 'activitypub' || protocol === 'mastodon') {
-    const url = new URL(comment.attributedTo)
-    const hostname = url.hostname
-    const segments = url.pathname.split('/')
-    const last = segments.pop() || segments.pop() // Handle potential trailing slash
-    if (hostname && last) {
-      username = `${last}@${hostname}`
-    }
-
-    const isAllowedImageOrigin = checkIfAllowedImageOrigin(commenter)
-    if (isAllowedImageOrigin) {
-      profileIcon = commenter.icon?.url || ''
-    }
-  } else if (protocol === 'twitter') {
-    username = commenter.fqUsername
-    profileIcon = commenter.icon?.url || ''
-  }
-
-  return { profileIcon, username }
-}
-
-const getAttachmentImage = (attachments: ThreadcapAttachment[]) => {
-  return attachments.find((attachment: ThreadcapAttachment) => attachment?.mediaType?.indexOf('image') === 0)
-}
-
-const convertThreadcapResponseToPVComment = (response: ThreadcapResponse) => {
-  const { commenters, nodes, protocol, roots } = response
-  const root = roots[0]
-  const rootNode = nodes[root]
-
-  const generatePVComment = (node: ThreadcapNode, protocol: string) => {
-    const { comment, replies } = node
-    const { attachments, content, published, url } = comment
-
-    const nestedReplies = Array.isArray(replies)
-      ? replies.map((replyUrl: string) => {
-          const nestedNode = nodes[replyUrl]
-          let pvComment = null
-          if (nestedNode && !nestedNode.commentError) {
-            pvComment = generatePVComment(nestedNode, protocol)
-          }
-          return pvComment
-        })
-      : []
-
-    const contentKeys = content && typeof content === 'object' ? Object.keys(content) : []
-    const contentLangKey = contentKeys[0]
-
-    let cleanedContent = contentLangKey ? decodeHtml(striptags(content[contentLangKey])) : ''
-    cleanedContent = removeUsernamesFromBeginningOfString(cleanedContent)
-    const attachmentImage = getAttachmentImage(attachments)
-    const { profileIcon, username } = parseUserInfo(comment, protocol, commenters)
-
-    const pvComment: PVComment = {
-      content: cleanedContent,
-      imageUrl: attachmentImage?.url || null,
-      profileIcon,
-      published,
-      replies: nestedReplies,
-      url,
-      username
-    }
-
-    return pvComment
-  }
-
-  const pvCommentThread = generatePVComment(rootNode, protocol)
-  return pvCommentThread
 }
