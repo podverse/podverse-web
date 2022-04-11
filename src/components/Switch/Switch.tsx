d2a6045492f9c5e657e209103b9e9cffbd6e711d
Mitch Downey <mitchldowney@gmail.com>
2022-04-11 00:07:45 -0500

    Try to work-around "___ cannot be used as a JSX component" error in Github action pipeline

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,11 +1,16 @@
 import classNames from 'classnames'
-import ReactSwitch from 'react-switch'
+
+// TODO: temporarily using require instead of require to work around a build error happening
+// in the Github action pipeline: "'PlayerAudio' cannot be used as a JSX component."
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const ReactSwitch = require('react-switch')
 
 type Props = {
   ariaLabel: string
   checked?: boolean
   onChange: any
 }
+
 export const Switch = ({ ariaLabel, checked, onChange }: Props) => {
   const switchClass = classNames(checked ? 'is-on' : 'is-off')
 
