e1a685bf42a343ec22db3785cc65938fa8bb07d3
Mitch Downey <mitchldowney@gmail.com>
2022-04-20 01:20:03 -0500

    Make the about page screen-reader accessible

--- src/components/DownloadAppButtons/DownloadAppButtons.tsx
+++ src/components/DownloadAppButtons/DownloadAppButtons.tsx
@@ -1,16 +1,19 @@
+import { useTranslation } from 'next-i18next'
 import { PV } from '~/resources'
 
 type Props = unknown
 
 export const DownloadAppButtons = (props: Props) => {
+  const { t } = useTranslation()
   return (
     <div className='download-app-buttons'>
-      <a className='download-on-the-app-store no-radius' href={PV.Config.APP_DOWNLOAD_ON_THE_APP_STORE_URL} />
-      <a className='get-it-on-google-play no-radius' href={PV.Config.APP_GET_IT_ON_GOOGLE_PLAY_URL}>
-        <img alt='Get it on Google Play' src={PV.RoutePaths.web.googlePlayStoreBadge} />
+      <a aria-label={t('Download on the App Store')} className='download-badge-download-on-the-app-store no-radius' href={PV.Config.APP_DOWNLOAD_ON_THE_APP_STORE_URL} />
+      <a aria-label={t('Download on the Google Play Store')} className='download-badge-get-it-on-google-play no-radius' href={PV.Config.APP_GET_IT_ON_GOOGLE_PLAY_URL}>
+        <img alt='' src={PV.RoutePaths.web.googlePlayStoreBadge} />
       </a>
       <a
-        className='get-it-on-fdroid no-radius'
+        aria-label={t('Download on the F-Droid Store')}
+        className='download-badge-get-it-on-fdroid no-radius'
         href={PV.Config.APP_GET_IT_ON_FDROID_URL}
         rel='noopener noreferrer'
         target='_blank'
