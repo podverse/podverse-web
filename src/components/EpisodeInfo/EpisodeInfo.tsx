5efc69232375df55f46d60048e8afb05cc7084c2
Mitch Downey <mitchldowney@gmail.com>
2022-04-30 23:23:08 -0500

    Hide clips on episode liveItem page

--- src/components/EpisodeInfo/EpisodeInfo.tsx
+++ src/components/EpisodeInfo/EpisodeInfo.tsx
@@ -12,15 +12,18 @@ type Props = {
 
 export const EpisodeInfo = ({ episode, includeMediaItemControls = false, noMarginBottom }: Props) => {
   const { t } = useTranslation()
+  const { liveItem } = episode
+  const isLiveItem = !!liveItem
   const summaryText = episode.description || episode.subtitle || t('No episode notes available')
   const episodeInfoClassName = classNames('episode-info', noMarginBottom ? 'no-margin-bottom' : '')
+  const hrClassName = classNames(isLiveItem ? 'display-none' : '')
 
   return (
     <div className={episodeInfoClassName}>
       <h2 tabIndex={0}>{t('Episode Notes')}</h2>
       <TruncatedText dangerouslySetInnerHtml lines={3} text={summaryText} />
       {includeMediaItemControls && <MediaItemControls buttonSize='large' episode={episode} />}
-      <hr />
+      <hr className={hrClassName} />
     </div>
   )
 }
