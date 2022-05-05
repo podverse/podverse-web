28c5326a477392196e08dc7e6578c0b74b8ab203
Mitch Downey <mitchldowney@gmail.com>
2022-05-04 21:22:30 -0500

    Update xmpp.tsx

--- pages/xmpp.tsx
+++ pages/xmpp.tsx
@@ -2,7 +2,7 @@ import { GetServerSideProps } from 'next'
 import { useTranslation } from 'next-i18next'
 import { Page } from '~/lib/utility/page'
 import { PV } from '~/resources'
-import { ColumnsWrapper, Footer, Meta, PageHeader, PageScrollableContent, PVLink } from '~/components'
+import { ColumnsWrapper, Footer, Meta, PageHeader, PageScrollableContent } from '~/components'
 import { getDefaultServerSideProps } from '~/services/serverSideHelpers'
 
 type ServerProps = Page
