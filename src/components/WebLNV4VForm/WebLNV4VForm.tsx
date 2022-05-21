a57a3742dd943a1535562662d63ab8ee990bca80
Mitch Downey <mitchldowney@gmail.com>
2022-05-21 01:04:52 -0500

    Add loading spinner to webln-v4v-wrapper

--- src/components/WebLNV4VForm/WebLNV4VForm.tsx
+++ src/components/WebLNV4VForm/WebLNV4VForm.tsx
@@ -1,7 +1,9 @@
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 import dynamic from 'next/dynamic'
 import type { Episode, Podcast, ValueTag } from 'podverse-shared'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useCookies } from 'react-cookie'
+import { Icon } from '~/components'
 import { PV } from '~/resources'
 const {
   V4V_APP_NAME,
@@ -21,6 +23,7 @@ type Props = {
 
 const WebLNV4VFormNoSSR = ({ episode, podcast, serverCookies, valueTag }: Props) => {
   const [cookies, setCookie] = useCookies([])
+  const [isLoading, setIsLoading] = useState<boolean>(true)
 
   /*
     For the server-side render, we have to parse the object out of the string.
@@ -39,16 +42,22 @@ const WebLNV4VFormNoSSR = ({ episode, podcast, serverCookies, valueTag }: Props)
     parsedWebLNV4VFormSettings || {}
 
   useEffect(() => {
+    window.addEventListener('WebLN-V4V-Has-Loaded', handleHasLoaded)
     window.addEventListener('WebLN-V4V-Terms-Accepted', handleAcceptedTermsEvent)
     window.addEventListener('WebLN-V4V-Terms-Rejected', handleRejectTermsEvent)
     window.addEventListener('WebLN-V4V-New-Default-Values', handleDefaultValueChangedEvent)
 
     return () => {
+      window.removeEventListener('WebLN-V4V-Has-Loaded', handleHasLoaded),
       window.removeEventListener('WebLN-V4V-Terms-Accepted', handleAcceptedTermsEvent),
         window.removeEventListener('WebLN-V4V-Terms-Rejected', handleRejectTermsEvent),
         window.removeEventListener('WebLN-V4V-New-Default-Values', handleDefaultValueChangedEvent)
     }
   }, [])
+
+  const handleHasLoaded = () => {
+    setIsLoading(false)
+  }
 
   const handleAcceptedTermsEvent = () => {
     const { weblnV4VSettings } = cookies
@@ -92,22 +101,33 @@ const WebLNV4VFormNoSSR = ({ episode, podcast, serverCookies, valueTag }: Props)
   }
 
   return (
-    <webln-v4v
-      app_name={V4V_APP_NAME}
-      app_recipient_custom_key={V4V_APP_RECIPIENT_CUSTOM_KEY}
-      app_recipient_custom_value={V4V_APP_RECIPIENT_CUSTOM_VALUE}
-      app_recipient_ln_address={V4V_APP_RECIPIENT_LN_ADDRESS}
-      app_recipient_value_default={defaultAppAmount || V4V_APP_RECIPIENT_VALUE_DEFAULT}
-      content_type='podcast'
-      has_accepted_terms={acceptedTerms}
-      has_rejected_terms={rejectedTerms}
-      podcast_episode_title={episode?.title}
-      podcast_podcast_index_id={podcast.podcastIndexId}
-      podcast_title={podcast.title}
-      recipient_value_default={defaultContentCreatorAmount || V4V_RECIPIENT_VALUE_DEFAULT}
-      sender_name={defaultSenderName || ''}
-      v4v_tag={JSON.stringify(valueTag)}
-    />
+    <div className='webln-v4v-wrapper'>
+      {
+        isLoading && (
+          <div className='loading-wrapper'>
+            <Icon faIcon={faSpinner} spin />
+          </div>
+        )
+      }
+      <div className={`${isLoading ? 'display-none' : ''}`}>
+        <webln-v4v
+          app_name={V4V_APP_NAME}
+          app_recipient_custom_key={V4V_APP_RECIPIENT_CUSTOM_KEY}
+          app_recipient_custom_value={V4V_APP_RECIPIENT_CUSTOM_VALUE}
+          app_recipient_ln_address={V4V_APP_RECIPIENT_LN_ADDRESS}
+          app_recipient_value_default={defaultAppAmount || V4V_APP_RECIPIENT_VALUE_DEFAULT}
+          content_type='podcast'
+          has_accepted_terms={acceptedTerms}
+          has_rejected_terms={rejectedTerms}
+          podcast_episode_title={episode?.title}
+          podcast_podcast_index_id={podcast.podcastIndexId}
+          podcast_title={podcast.title}
+          recipient_value_default={defaultContentCreatorAmount || V4V_RECIPIENT_VALUE_DEFAULT}
+          sender_name={defaultSenderName || ''}
+          v4v_tag={JSON.stringify(valueTag)}
+        />
+      </div>
+    </div>
   )
 }
 
