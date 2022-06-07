0e40e8be7c3d657f9f2c6103bb5cc94a68eef6fe
Mitch Downey <mitchldowney@gmail.com>
2022-06-06 23:58:33 -0500

    Update alternateEnclosures.ts

--- src/lib/utility/alternateEnclosures.ts
+++ src/lib/utility/alternateEnclosures.ts
@@ -1,4 +1,4 @@
-import { EpisodeAlternateEnclosure, EpisodeAlternateEnclosureSource, NowPlayingItem } from 'podverse-shared'
+import { EpisodeAlternateEnclosure, EpisodeAlternateEnclosureSource } from 'podverse-shared'
 
 export const generateAlternateEnclosureDropdownOptions = (alternateEnclosures: EpisodeAlternateEnclosure[]) => {
   const dropdownOptions = []
