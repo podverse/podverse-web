a0561556dfe6d29d5365cbf2245d481f101bf296
Mitch Downey <mitchldowney@gmail.com>
2022-07-17 18:34:32 -0500

    Add Access-Control-Allow-Origin * for the /embed/player path response headers

--- next.config.js
+++ next.config.js
@@ -19,6 +19,20 @@ const sentryWebpackPluginOptions = {
 }
 
 const moduleExports = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/embed/player",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  },
   reactStrictMode: true,
   i18n,
   sentry: {
