8e1ff953b612475eca7e23d469ae445e33a6a141
Mitch Downey <mitchldowney@gmail.com>
2022-07-18 23:38:26 -0500

    Use translation locales in moment.js dates

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   i18n: {
     defaultLocale: 'en',
+    // NOTE: Make sure to update locales in the ./src/lib/utility/date.ts file
+    // for moment.js translations.
     locales: ['de', 'el', 'en', 'es', 'fr', 'lt', 'nb-NO', 'pt', 'ru', 'sv', 'tr']
   },
   react: {
