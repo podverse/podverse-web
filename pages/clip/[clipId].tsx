476504aec70d6542002365f87dc5de1dbb770902
Mitch Downey <mitchldowney@gmail.com>
2022-11-02 21:45:31 -0500

    Update [clipId].tsx

--- pages/clip/[clipId].tsx
+++ pages/clip/[clipId].tsx
@@ -1,12 +1,7 @@
 import { GetServerSideProps } from 'next'
 import { useTranslation } from 'next-i18next'
 import OmniAural, { useOmniAural } from 'omniaural'
-import {
-  addLightningBoltToString,
-  checkIfVideoFileOrVideoLiveType,
-  Episode,
-  MediaRef
-} from 'podverse-shared'
+import { addLightningBoltToString, checkIfVideoFileOrVideoLiveType, Episode, MediaRef } from 'podverse-shared'
 import { getLightningKeysendValueItem } from 'podverse-shared'
 import { useEffect, useRef, useState } from 'react'
 import {
