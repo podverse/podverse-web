7be6a5fc0c88cf60b1b7d07cd12da6de69137460
Mitch Downey <mitchldowney@gmail.com>
2023-04-14 23:57:27 -0500

    Fix incorrect date translations
    #1090

--- src/lib/utility/locales.ts
+++ src/lib/utility/locales.ts
@@ -1,8 +1,21 @@
-export function getLang() {
-  if (typeof navigator !== 'undefined') {
-    if (navigator.languages != undefined) return navigator.languages[0]
-    return navigator.language
+// Remember to update src/lib/utility/date.ts as well
+const defaultLang = 'en'
+const enabledLangs = [
+  'da', 'de', 'el', 'es', 'fr', 'lt', 'nb',
+  'pt', 'pt-br', 'ru', 'sv', 'tr'
+]
+let enabledLang = defaultLang
+
+if (typeof navigator !== 'undefined') {
+  if (navigator.languages != undefined) {
+    if (enabledLangs.some(x => x === navigator.languages[0]?.toLowerCase())) {
+      enabledLang = navigator.languages[0]
+    } else if (enabledLangs.some(x => x === navigator.language?.toLowerCase())) {
+      enabledLang = navigator.language
+    }
   } else {
-    return 'en'
+    enabledLang = defaultLang
   }
 }
+
+export const currentLocale = enabledLang
