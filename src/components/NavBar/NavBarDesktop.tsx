97b7a0fe3c782da35c4681c15f12a23c2ed69df1
Mitch Downey <mitchldowney@gmail.com>
2025-07-11 23:19:38 -0500

    feat: refactor NavBarDesktop component by extracting NavBarLeftButtons and NavBarProfileDropdown for improved readability and maintainability

--- src/components/NavBar/NavBarDesktop.tsx
+++ src/components/NavBar/NavBarDesktop.tsx
@@ -1,10 +1,32 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import React, { useContext } from "react";
+import { useRouter } from "next/navigation";
 import { FaChevronDown, FaChevronLeft, FaChevronRight, FaRegUserCircle, FaUserCircle } from "react-icons/fa";
 import { AccountContext } from "../../contexts/Account";
 import styles from "../../styles/components/NavBar/NavBarDesktop.module.scss";
+
+function NavBarLeftButtons({ onBack, onForward }: { onBack: () => void; onForward: () => void }) {
+  return (
+    <div className={styles.leftButtons}>
+      <button className={`${styles.navButton} ${styles.navButtonLeft}`} onClick={onBack} aria-label="Back">
+        <FaChevronLeft />
+      </button>
+      <button className={`${styles.navButton} ${styles.navButtonRight}`} onClick={onForward} aria-label="Forward">
+        <FaChevronRight />
+      </button>
+    </div>
+  );
+}
+
+function NavBarProfileDropdown({ isLoggedIn, onClick }: { isLoggedIn: boolean; onClick: () => void }) {
+  return (
+    <button className={styles.dropdownButton} onClick={onClick}>
+      {isLoggedIn ? <FaRegUserCircle className={styles.profileIcon} /> : <FaUserCircle className={styles.profileIcon} />}
+      <FaChevronDown />
+    </button>
+  );
+}
 
 const NavBarDesktop: React.FC = () => {
   const { isLoggedIn } = useContext(AccountContext);
@@ -14,28 +36,10 @@ const NavBarDesktop: React.FC = () => {
     console.log("Dropdown button pressed");
   };
 
-  const handleBack = () => {
-    router.back();
-  };
-
-  const handleForward = () => {
-    router.forward();
-  };
-
   return (
     <nav className={styles.navbar}>
-      <div className={styles.leftButtons}>
-        <button className={`${styles.navButton} ${styles.navButtonLeft}`} onClick={handleBack} aria-label="Back">
-          <FaChevronLeft />
-        </button>
-        <button className={`${styles.navButton} ${styles.navButtonRight}`} onClick={handleForward} aria-label="Forward">
-          <FaChevronRight />
-        </button>
-      </div>
-      <button className={styles.dropdownButton} onClick={handleDropdownClick}>
-        {isLoggedIn ? <FaRegUserCircle className={styles.profileIcon} /> : <FaUserCircle className={styles.profileIcon} />}
-        <FaChevronDown />
-      </button>
+      <NavBarLeftButtons onBack={router.back} onForward={router.forward} />
+      <NavBarProfileDropdown isLoggedIn={isLoggedIn} onClick={handleDropdownClick} />
     </nav>
   );
 };
