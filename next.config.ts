0543098f5ea88cdd63d704944347e3fef9324c77
Mitch Downey <mitchldowney@gmail.com>
2025-07-16 15:51:28 -0500

    feat: add additional remote pattern for HTTP images in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: '**',
       },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
     ],
   },
 };
