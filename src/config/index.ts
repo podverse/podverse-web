49a55a866e37275aec685815eb513e4dc231b544
Mitch Downey <mitchldowney@gmail.com>
2025-07-23 00:26:49 -0500

    feat: update API configuration to clarify variable exposure and rename internal/external to private/public

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,21 +1,23 @@
 export const config = {
   api: {
     /*
-      Example:
+      NOTE: Remember to expose the public env vars in next.config.js
+
+      Development mode defaults:
         API_PROTOCOL=http
         API_HOST=localhost
         API_PORT=1234
         API_PREFIX=/api
         API_VERSION=/v2
     */
-    internal: {
+    private: {
       protocol: process.env.API_PROTOCOL,
       host: process.env.API_HOST,
       port: process.env.API_PORT,
       prefix: process.env.API_PREFIX,
       version: process.env.API_VERSION
     },
-    external: {
+    public: {
       protocol: process.env.NEXT_PUBLIC_API_PROTOCOL,
       host: process.env.NEXT_PUBLIC_API_HOST,
       port: process.env.NEXT_PUBLIC_API_PORT,
--- src/factories/apiRequestService.ts
+++ src/factories/apiRequestService.ts
@@ -3,19 +3,19 @@ import { config } from "../config";
 
 export function getSSRApiRequestService(jwt?: string | null): ApiRequestService {
   return new ApiRequestService({
-    protocol: config.api.internal.protocol || '',
-    host: config.api.internal.host || '',
-    port: config.api.internal.port || '',
-    prefix: config.api.internal.prefix || '',
-    version: config.api.internal.version || '',
+    protocol: config.api.private.protocol || '',
+    host: config.api.private.host || '',
+    port: config.api.private.port || '',
+    prefix: config.api.private.prefix || '',
+    version: config.api.private.version || '',
     ...(jwt ? { jwt } : {})
   });
 }
 
 export const apiRequestService = new ApiRequestService({
-  protocol: config.api.external.protocol || '',
-  host: config.api.external.host || '',
-  port: config.api.external.port || '',
-  prefix: config.api.external.prefix || '',
-  version: config.api.external.version || ''
+  protocol: config.api.public.protocol || '',
+  host: config.api.public.host || '',
+  port: config.api.public.port || '',
+  prefix: config.api.public.prefix || '',
+  version: config.api.public.version || ''
 });
