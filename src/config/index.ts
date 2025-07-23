6d40b65309b6ce90e10c6d7a458f05cd66282d1a
Mitch Downey <mitchldowney@gmail.com>
2025-07-22 22:02:41 -0500

    feat: restructure API configuration to separate internal and external settings

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,18 @@
 export const config = {
   api: {
-    protocol: process.env.NEXT_PUBLIC_API_PROTOCOL || 'http',
-    host: process.env.NEXT_PUBLIC_API_HOST || 'localhost',
-    port: process.env.NEXT_PUBLIC_API_PORT || '1234',
-    prefix: process.env.NEXT_PUBLIC_API_PREFIX || '/api',
-    version: process.env.NEXT_PUBLIC_API_VERSION || '/v2'
+    internal: {
+      protocol: process.env.API_PROTOCOL || 'http',
+      host: process.env.API_HOST || 'localhost',
+      port: process.env.API_PORT || '1234',
+      prefix: process.env.API_PREFIX || '/api',
+      version: process.env.API_VERSION || '/v2'
+    },
+    external: {
+      protocol: process.env.NEXT_PUBLIC_API_PROTOCOL || 'http',
+      host: process.env.NEXT_PUBLIC_API_HOST || 'localhost',
+      port: process.env.NEXT_PUBLIC_API_PORT || '1234',
+      prefix: process.env.NEXT_PUBLIC_API_PREFIX || '/api',
+      version: process.env.NEXT_PUBLIC_API_VERSION || '/v2'
+    }
   }
-}
+};
--- src/factories/apiRequestService.ts
+++ src/factories/apiRequestService.ts
@@ -3,19 +3,19 @@ import { config } from "../config";
 
 export function getSSRApiRequestService(jwt?: string | null): ApiRequestService {
   return new ApiRequestService({
-    protocol: config.api.protocol,
-    host: config.api.host,
-    port: config.api.port,
-    prefix: config.api.prefix,
-    version: config.api.version,
+    protocol: config.api.internal.protocol,
+    host: config.api.internal.host,
+    port: config.api.internal.port,
+    prefix: config.api.internal.prefix,
+    version: config.api.internal.version,
     ...(jwt ? { jwt } : {})
   });
 }
 
 export const apiRequestService = new ApiRequestService({
-  protocol: config.api.protocol,
-  host: config.api.host,
-  port: config.api.port,
-  prefix: config.api.prefix,
-  version: config.api.version
+  protocol: config.api.external.protocol,
+  host: config.api.external.host,
+  port: config.api.external.port,
+  prefix: config.api.external.prefix,
+  version: config.api.external.version
 });
