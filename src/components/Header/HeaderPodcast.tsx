42895bddfa6402d6fd4dd220af95f5cb84d9d041
Mitch Downey <mitchldowney@gmail.com>
2025-09-11 16:11:55 -0500

    refactor: enhance HeaderPodcast to use findDTOChannelImageBySize for image selection

--- src/components/Header/HeaderPodcast.tsx
+++ src/components/Header/HeaderPodcast.tsx
@@ -1,28 +1,32 @@
-import { DTOChannel } from "podverse-helpers";
+import { DTOChannel, findDTOChannelImageBySize } from "podverse-helpers";
 import React from "react";
 import styles from "../../styles/components/Header/HeaderPodcast.module.scss";
 import Image from "../Image/Image";
-import { IMAGE_HEADER_SQUARE_SIZE } from "../../constants/images";
+import { IMAGE_HEADER_SQUARE_SIZE, IMAGE_HEADER_SQUARE_SIZE_FIND_TARGET } from "../../constants/images";
 
 type HeaderPodcastProps = {
   channel: DTOChannel;
 };
 
-const HeaderPodcast: React.FC<HeaderPodcastProps> = ({ channel }) => (
-  <header className={styles.header}>
-    <div className={styles.headerContent}>
-      <div className={styles.headerImageWrapper}>
-        <Image
-          src={channel.channel_images?.[0]?.url}
-          alt={channel.title || "Podcast Image"}
-          width={IMAGE_HEADER_SQUARE_SIZE}
-          height={IMAGE_HEADER_SQUARE_SIZE}
-          className={styles.image}
-        />
+const HeaderPodcast: React.FC<HeaderPodcastProps> = ({ channel }) => {
+  const channel_image = findDTOChannelImageBySize(channel.channel_images, IMAGE_HEADER_SQUARE_SIZE_FIND_TARGET, 'greater');
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.headerContent}>
+        <div className={styles.headerImageWrapper}>
+          <Image
+            src={channel_image?.url}
+            alt={channel.title || "Podcast Image"}
+            width={IMAGE_HEADER_SQUARE_SIZE}
+            height={IMAGE_HEADER_SQUARE_SIZE}
+            className={styles.image}
+          />
+        </div>
+        <h1 className={styles.title}>{channel.title}</h1>
       </div>
-      <h1 className={styles.title}>{channel.title}</h1>
-    </div>
-  </header>
-);
+    </header>
+  )
+};
 
 export default HeaderPodcast;
