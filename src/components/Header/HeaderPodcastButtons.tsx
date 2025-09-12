9995e215f33aff88f0ebfc1f88ebcb8541924da1
Mitch Downey <mitchldowney@gmail.com>
2025-09-11 22:54:50 -0500

    refactor: rename "home_page" to "website" in translations and update HeaderPodcastButtons to include website link

--- src/components/Header/HeaderPodcastButtons.tsx
+++ src/components/Header/HeaderPodcastButtons.tsx
@@ -2,7 +2,7 @@
 
 import { DTOChannel } from "podverse-helpers";
 import React from "react";
-import { FaRss } from "react-icons/fa";
+import { FaGlobe, FaRss } from "react-icons/fa";
 import styles from "../../styles/components/Header/HeaderPodcastButtons.module.scss";
 import Link from "../Link/Link";
 import { useTranslations } from "next-intl";
@@ -19,6 +19,7 @@ const HeaderPodcastButtons: React.FC<HeaderPodcastButtonsProps> = ({ channel }) 
       {
         channel?.feed?.url && (
           <Link
+            className={styles.button}
             href={channel.feed.url}
             target="_blank"
             rel="noopener noreferrer"
@@ -26,6 +27,20 @@ const HeaderPodcastButtons: React.FC<HeaderPodcastButtonsProps> = ({ channel }) 
             title={tInfo("rss_feed")}
             color="secondary">
             <FaRss />
+          </Link>
+        )
+      }
+      {
+        channel?.channel_about?.website_link_url && (
+          <Link
+            className={styles.button}
+            href={channel.channel_about.website_link_url}
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label={tInfo("website")}
+            title={tInfo("website")}
+            color="secondary">
+            <FaGlobe />
           </Link>
         )
       }
