c71a4bb677d50416cba4b0707b75a7b44897cb43
Mitch Downey <mitchldowney@gmail.com>
2025-09-17 19:51:52 -0500

    feat: add Divider component to PodcastSideContent; adjust styles in ContentPeople and ContentPeopleRow for improved layout

--- src/app/podcast/[channel_id]/PodcastSideContent.tsx
+++ src/app/podcast/[channel_id]/PodcastSideContent.tsx
@@ -2,6 +2,7 @@ import { DTOChannel } from "podverse-helpers";
 import { SideContent } from "../../../components/SideContent/SideContent";
 import { ContentAbout } from "../../../components/Content/About/ContentAbout";
 import { ContentPeople } from "../../../components/Content/People/ContentPeople";
+import Divider from "../../../components/Divider/Divider";
 
 type PodcastSideContentProps = {
   channel: DTOChannel;
@@ -13,10 +14,12 @@ export const PodcastSideContent = ({ channel }: PodcastSideContentProps) => {
       <ContentAbout
         defaultOpen={true}
         description={channel.channel_description?.value} />
+      <Divider />
       <ContentPeople
         defaultOpen={true}
         channel_persons={channel?.channel_persons}
       />
+      <Divider />
     </SideContent>
   )
 }
