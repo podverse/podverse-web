dd22b9f2bf17f42665be10b57a04d0fd5038a083
Mitch Downey <mitchldowney@gmail.com>
2025-09-19 20:36:37 -0500

    feat: enhance PlayButton component with play/pause functionality and update font size in styles

--- src/components/MediaPlayer/Buttons/PlayButton.tsx
+++ src/components/MediaPlayer/Buttons/PlayButton.tsx
@@ -1,8 +1,20 @@
-import { FaPlay } from "react-icons/fa6"
+import { FaPause, FaPlay } from "react-icons/fa6"
 import styles from "../../../styles/components/MediaPlayer/Buttons/PlayButton.module.scss"
+import { useMediaPlayer } from "../../../contexts/MediaPlayer"
 
 export const PlayButton = () => {
+  const { mpIsPlaying, setMPIsPlaying } = useMediaPlayer();
+  const icon = mpIsPlaying ? <FaPause /> : <FaPlay />;
+
+  const handleClick = () => {
+    setMPIsPlaying(!mpIsPlaying);
+  };
+
   return (
-    <button className={styles.playButton}><FaPlay /></button>
+    <button
+      className={styles.playButton}
+      onClick={handleClick}>
+      {icon}
+    </button>
   )
 }
