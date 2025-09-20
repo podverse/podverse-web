f03d246f43196058104d58cb282a6e14435fc8da
Mitch Downey <mitchldowney@gmail.com>
2025-09-19 20:43:01 -0500

    feat: remove unused imports from ContentPodrollRows component

--- src/components/Content/Podroll/ContentPodrollRows.tsx
+++ src/components/Content/Podroll/ContentPodrollRows.tsx
@@ -1,6 +1,4 @@
 import { DTOPodroll } from "podverse-helpers"
-import Accordion from "../../Accordian/Accordian";
-import { ContentPodrollHeader } from "./ContentPodrollHeader";
 import styles from "../../../styles/components/Content/Podroll/ContentPodroll.module.scss";
 import { ContentPodrollChannelRow } from "./ContentPodrollChannelRow";
 import { ContentPodrollItemRow } from "./ContentPodrollItemRow";
