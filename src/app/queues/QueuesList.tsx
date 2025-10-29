8d89e4b2202d2db0a8480de1df4d2f8e82aea07c
Mitch Downey <mitchldowney@gmail.com>
2025-10-28 23:28:11 -0500

    feat: add "use client" directive to QueuesList component for improved rendering

--- src/app/queues/QueuesList.tsx
+++ src/app/queues/QueuesList.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import LoadingSpinnerOverlay from "../../components/LoadingSpinner/LoadingSpinnerOverlay";
 import { ListQueueResources } from "../../components/List/Queues/ListQueueResources";
