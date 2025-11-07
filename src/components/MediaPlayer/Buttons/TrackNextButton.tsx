44e79de04cd6a70901dee15586c26e7028cf2731
Mitch Downey <mitchldowney@gmail.com>
2025-11-06 23:11:32 -0600

    feat: Implement next track functionality in TrackNextButton component

--- src/components/MediaPlayer/Buttons/TrackNextButton.tsx
+++ src/components/MediaPlayer/Buttons/TrackNextButton.tsx
@@ -1,9 +1,29 @@
+import { useEffect, useRef } from "react";
 import { FaAnglesRight } from "react-icons/fa6"
+import { useQueueResourcesLoadActive } from "../../../hooks/useQueueResourcesLoadActive";
+import { useQueueResourcesMoveNowPlayingToHistory } from "../../../hooks/useQueueResourceMoveNowPlayingToHistory";
+import { useMediaPlayer } from "../../../contexts/MediaPlayer";
 import styles from "../../../styles/components/MediaPlayer/Buttons/TrackNextButton.module.scss"
 
 export const TrackNextButton = () => {
-  const onClick = () => {
-    alert("Next track functionality to be implemented")
+  const { mpItem, setMPShouldPlay } = useMediaPlayer();
+  const moveNowPlayingToHistory = useQueueResourcesMoveNowPlayingToHistory();
+  const queueResourcesLoadActive = useQueueResourcesLoadActive();
+
+  const mpItemRef = useRef(mpItem);
+
+  useEffect(() => {
+    mpItemRef.current = mpItem;
+  }, [mpItem]);
+
+  const onClick = async () => {
+    await moveNowPlayingToHistory({
+      mpClip: null,
+      mpItem: mpItemRef.current,
+      mpItemSoundbite: null
+    });
+    setMPShouldPlay(true);
+    await queueResourcesLoadActive();
   }
 
   return (
