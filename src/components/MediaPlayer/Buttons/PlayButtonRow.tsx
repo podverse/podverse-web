2fa77f2affc6951df750d55605bd7e24a85d534c
Mitch Downey <mitchldowney@gmail.com>
2025-11-06 23:10:24 -0600

    fix: Correct import order in PlayButtonRow component

--- src/components/MediaPlayer/Buttons/PlayButtonRow.tsx
+++ src/components/MediaPlayer/Buttons/PlayButtonRow.tsx
@@ -1,8 +1,8 @@
 import { useTranslations } from "next-intl";
 import { FaPause, FaPlay } from "react-icons/fa6";
 import { useMediaPlayer } from "../../../contexts/MediaPlayer";
-import styles from "../../../styles/components/MediaPlayer/Buttons/PlayButtonRow.module.scss"
 import { DTOClip, DTOItem, DTOItemChapter, DTOItemSoundbite } from "podverse-helpers";
+import styles from "../../../styles/components/MediaPlayer/Buttons/PlayButtonRow.module.scss"
 
 type PlayButtonRowProps = {
   clip?: DTOClip;
