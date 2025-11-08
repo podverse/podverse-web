db9c2218107b916a726a1a75c8e9665cc857a997
Mitch Downey <mitchldowney@gmail.com>
2025-11-08 14:27:24 -0600

    feat: Update Button component import and utilize ButtonVariant in LiveItemStatus

--- src/components/LiveItem/LiveItemStatus.tsx
+++ src/components/LiveItem/LiveItemStatus.tsx
@@ -1,6 +1,6 @@
 import { useTranslations } from "next-intl";
 import { DTOLiveItem, LiveItemStatusEnum } from "podverse-helpers";
-import { Button } from "../Button/Button";
+import { Button, ButtonVariant } from "../Button/Button";
 import styles from "../../styles/components/LiveItem/LiveItemStatus.module.scss";
 
 type LiveItemStatusProps = {
@@ -11,16 +11,19 @@ export const LiveItemStatus = ({ live_item }: LiveItemStatusProps) => {
   const tMedia = useTranslations("media");
 
   let statusText = tMedia("livestream.scheduled");
+  let variant: ButtonVariant = 'miniGlow';
   if (live_item.live_item_status.id === LiveItemStatusEnum.Live) {
     statusText = tMedia("livestream.live_now");
+    variant = 'miniGlowDanger';
   } else if (live_item.live_item_status.id === LiveItemStatusEnum.Ended) {
     statusText = tMedia("livestream.ended");
+    variant = 'miniGlowWarning';
   }
 
   return (
     <Button
       className={styles.button}
-      variant="miniGlowDanger">
+      variant={variant}>
       {statusText}
     </Button>
   );
