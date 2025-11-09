9f6028be19a2656e8dd5bf4e7feda93baf8fd821
Mitch Downey <mitchldowney@gmail.com>
2025-11-09 16:22:51 -0600

    feat: Add ImageNonReact component for image handling with optional proxy support

---
+++ src/components/Image/ImageNonReact.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import classNames from 'classnames';
+import React, { useState } from "react";
+import { PROXY } from '../../constants/proxy';
+import { IMAGES } from '../../constants/images';
+import styles from "../../styles/components/Image/ImageNonReact.module.scss";
+
+interface ImageNonReactProps {
+  src?: string | null;
+  alt: string;
+  className?: string;
+  skipProxy?: boolean;
+}
+
+export const ImageNonReact: React.FC<ImageNonReactProps> = ({
+  src,
+  alt,
+  className,
+  skipProxy
+}) => {
+  const [imageError, setImageError] = useState(false);
+
+  if (!src || imageError) {
+    return (
+      <img
+        src={IMAGES.SRC.PLACEHOLDER}
+        alt={alt}
+        className={classNames(styles.imagePlaceholder, className)}
+      />
+    );
+  }
+
+  const finalSrc = skipProxy ? src : PROXY.PATH + src;
+
+  return (
+    <img
+      src={finalSrc}
+      alt={alt}
+      className={className}
+      onError={() => setImageError(true)}
+    />
+  );
+};
