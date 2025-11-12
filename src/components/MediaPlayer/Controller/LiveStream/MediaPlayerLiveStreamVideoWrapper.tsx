15d09ac12f3d60f154e2bdbd0e165236db508a8d
Mitch Downey <mitchldowney@gmail.com>
2025-11-12 12:43:18 -0600

    refactor: Remove unused imports and commented code from MediaPlayerLiveStreamVideoWrapper component

--- src/components/MediaPlayer/Controller/LiveStream/MediaPlayerLiveStreamVideoWrapper.tsx
+++ src/components/MediaPlayer/Controller/LiveStream/MediaPlayerLiveStreamVideoWrapper.tsx
@@ -1,15 +1,9 @@
 "use client";
 
-// import { getSelectedItemEnclosureUrl } from "podverse-helpers";
 import { MediaPlayerControllerLiveStreamVideo } from "./MediaPlayerControllerLiveStreamVideo";
 import { MediaPlayerLivestreamVideoPortalFloating } from "./MediaPlayerLivestreamVideoPortalFloating";
-// import { checkIfIsVideoFile, checkIsLiveItem } from "../../../../utils/mediaPlayer/mediaPlayerItemEnclosureType";
 
 export function MediaPlayerLiveStreamVideoWrapper() {
-  // const selectedItemEnclosureUrl = getSelectedItemEnclosureUrl(mpItem?.item_enclosures ?? []);
-  // const isVideoFile = checkIfIsVideoFile(selectedItemEnclosureUrl);
-  // const isLiveItem = checkIsLiveItem(mpItem);
-
   return (
     <MediaPlayerLivestreamVideoPortalFloating>
       <MediaPlayerControllerLiveStreamVideo />
