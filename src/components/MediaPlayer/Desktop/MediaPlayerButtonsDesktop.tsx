f70e37ffc6cc5db09eb4887f1a6a080f31ec04ca
Mitch Downey <mitchldowney@gmail.com>
2025-11-15 20:01:18 -0600

    fix: Remove unused PlaybackSpeedButton from MediaPlayerButtonsDesktop component

--- src/components/MediaPlayer/Desktop/MediaPlayerButtonsDesktop.tsx
+++ src/components/MediaPlayer/Desktop/MediaPlayerButtonsDesktop.tsx
@@ -1,6 +1,5 @@
 import { ClipButton } from "../Buttons/ClipButton";
 import { MuteButton } from "../Buttons/MuteButton";
-import { PlaybackSpeedButton } from "../Buttons/PlaybackSpeedButton";
 import { PlaylistAddToButton } from "../Buttons/PlaylistAddToButton";
 import { VolumeSlider } from "../Sliders/VolumeSlider";
 import styles from "../../../styles/components/MediaPlayer/Desktop/MediaPlayerButtonsDesktop.module.scss";
@@ -11,7 +10,6 @@ export const MediaPlayerButtonsDesktop = () => {
     <div className={styles.buttons}>
       <div className={styles.startSection}>
         <PlaylistAddToButton />
-        <PlaybackSpeedButton />
         <ClipButton />
         <SettingsButton />
       </div>
