b566773380a73e0824f559cfd6052fb5254e05d8
Mitch Downey <mitchldowney@gmail.com>
2025-11-15 20:01:41 -0600

    feat: Add SettingsButton to MediaPlayerButtonsMobile component

--- src/components/MediaPlayer/Mobile/MediaPlayerButtonsMobile.tsx
+++ src/components/MediaPlayer/Mobile/MediaPlayerButtonsMobile.tsx
@@ -3,11 +3,13 @@
 import { JumpBackButtonMobile } from "../Buttons/JumpBackButtonMobile";
 import { JumpForwardButtonMobile } from "../Buttons/JumpForwardButtonMobile";
 import { PlayButtonMobile } from "../Buttons/PlayButtonMobile";
+import { SettingsButton } from "../Buttons/SettingsButton";
 import styles from "../../../styles/components/MediaPlayer/Mobile/MediaPlayerButtonsMobile.module.scss";
 
 export const MediaPlayerButtonsMobile = () => {
   return (
     <div className={styles.buttons}>
+      <SettingsButton />
       <JumpBackButtonMobile />
       <JumpForwardButtonMobile />
       <PlayButtonMobile />
