b49a768686d4634491390f16c3d7668d3b82d770
Mitch Downey <mitchldowney@gmail.com>
2025-11-16 19:02:18 -0600

    feat: Enhance PodcastIndexFeedPage with SSR handling and redirection logic

--- src/app/podcast-index/feed/[podcast_index_id]/page.tsx
+++ src/app/podcast-index/feed/[podcast_index_id]/page.tsx
@@ -1,11 +1,44 @@
 import React from "react";
 import { PodcastIndexFeedClient } from "./PodcastIndexFeedClient";
 import { apiRequestService } from "../../../../factories/apiRequestService";
+import { notFound, redirect } from "next/navigation";
+import { DTOChannel, MediumEnum } from "podverse-helpers";
 
-export default async function PodcastIndexFeed({ params }: { params: { podcast_index_id: string } }) {
-  const podcastIndexResponse = await apiRequestService.reqPodcastIndexFeedById(params.podcast_index_id);
+export default async function PodcastIndexFeedPage(props: { params: Promise<{ podcast_index_id: string }> }) {
+  const { podcast_index_id } = await props.params;
+  const podcastIndexResponse = await apiRequestService.reqPodcastIndexFeedById(podcast_index_id);
 
   const ssrFeed = podcastIndexResponse.feed;
+
+  if (!ssrFeed) {
+    return notFound();
+  }
+
+  let redirectPath: string | null = null;
+  let ssrChannel: DTOChannel | null = null;
+  try {
+    // Always returns 200, even if not found, to avoid NEXT SSR error log
+    ssrChannel = await apiRequestService.reqChannelGetByPodcastIndexId(podcast_index_id);
+  } catch (error) {
+    // If the channel is not found, we can ignore the error
+  }
+
+  if (ssrChannel) {
+    const medium_id = ssrChannel.medium_id;
+    if (medium_id === MediumEnum.Podcast) {
+      redirectPath = `/podcast/${ssrChannel.id_text}`;
+    } else if (medium_id === MediumEnum.Video) {
+      redirectPath = `/video/${ssrChannel.id_text}`;
+    } else if (medium_id === MediumEnum.Music) {
+      redirectPath = `/music/${ssrChannel.id_text}`;
+    } else {
+      console.log("No redirect: medium_id did not match Podcast, Video, or Music.");
+    }
+  }
+
+  if (redirectPath) {
+    redirect(redirectPath);
+  }
 
   return (
     <PodcastIndexFeedClient ssrFeed={ssrFeed} />
