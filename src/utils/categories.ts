841b284703d06394be7a63416f035e188fb220b7
Mitch Downey <mitchldowney@gmail.com>
2025-11-23 23:52:32 -0600

    feat: Implement onClickCategory function for category selection handling

---
+++ src/utils/categories.ts
@@ -0,0 +1,32 @@
+import { DTOCategory, QUERY_PARAMS_GLOBAL_GET_MANY_SORT_VALUES } from "podverse-helpers";
+
+type OnClickCategoryParams = {
+  category: DTOCategory,
+  setFilterParams: (params: any) => void,
+  filterParams: any,
+  setShowCategoriesModal: (isOpen: boolean) => void,
+  linkPath?: string,
+  router: any
+};
+
+export const onClickCategory = ({
+  category,
+  setFilterParams,
+  filterParams,
+  setShowCategoriesModal,
+  linkPath,
+  router
+}: OnClickCategoryParams) => {
+  if (category?.mapping_key) {
+    setFilterParams({
+      ...filterParams,
+      type: "category",
+      sort: QUERY_PARAMS_GLOBAL_GET_MANY_SORT_VALUES
+        .includes(filterParams.sort) ? filterParams.sort : "top",
+      category: category.mapping_key,
+      page: 1
+    });
+    setShowCategoriesModal(false);
+    router.replace(`${linkPath}?category=${category.mapping_key}`);
+  }
+};
