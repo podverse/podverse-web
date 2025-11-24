7557129ddddfaa8ce9325c1e45ea1c5c50ccddd2
Mitch Downey <mitchldowney@gmail.com>
2025-11-23 23:52:49 -0600

    refactor: Remove CategoriesClient and PodcastsCategoriesPage components

--- src/app/podcasts/categories/page.tsx
+++
@@ -1,9 +0,0 @@
-import CategoriesClient from "../../../clients/CategoriesClient";
-
-export default async function PodcastsCategoriesPage() {
-  return (
-    <CategoriesClient
-      titleKey="podcast.podcasts"
-      linkPath="/podcasts" />
-  );
-}
