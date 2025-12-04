55e2acb1965cd2cc487ccb6ccdab5135036ab1a4
Mitch Downey <mitchldowney@gmail.com>
2025-12-03 20:47:56 -0600

    feat: Refactor Footer component to use translations for links and social media labels

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,47 +1,57 @@
+"use client";
+
 import Link from "next/link";
+import { SiMatrix } from "react-icons/si";
 import styles from "../../styles/components/Footer/Footer.module.scss";
 import FooterBrand from "./FooterBrand";
 import FooterCopyright from "./FooterCopyright";
 import { ROUTES } from "../../constants/routes";
 import { FaDiscord, FaGithub, FaMastodon, FaXTwitter } from "react-icons/fa6";
-import { SiMatrix } from "react-icons/si";
 import { SOCIALS } from "../../constants/socials";
+import { useTranslations } from "next-intl";
 
-const Footer: React.FC = () => (
-  <footer className={styles.footer}>
-    <div className={styles.footerTop}>
-      <FooterBrand />
-      <FooterCopyright />
-    </div>
-    <div className={styles.footerBottom}>
-      <div className={styles.footerLinks}>
-        <Link href={ROUTES.CONTACT}>Contact</Link>
-        <Link href={ROUTES.ABOUT}>About</Link>
-        <Link href={ROUTES.TERMS}>Terms</Link>
-        <Link href={ROUTES.MEMBERSHIP}>Premium</Link>
-        <Link href={ROUTES.MOBILE_APP}>Mobile</Link>
-        <Link href={ROUTES.EMBED}>Embed</Link>
-        <Link href={ROUTES.DONATE}>Donate</Link>
-      </div>
-      <div className={styles.footerSocialLinks}>
-        <Link href={SOCIALS.DISCORD} target="_blank" rel="noopener noreferrer" aria-label="Discord">
-          <FaDiscord />
-        </Link>
-        <Link href={SOCIALS.ACTIVITY_PUB} target="_blank" rel="noopener noreferrer" aria-label="Mastodon">
-          <FaMastodon />
-        </Link>
-        <Link href={SOCIALS.X} target="_blank" rel="noopener noreferrer" aria-label="X">
-          <FaXTwitter />
-        </Link>
-        <Link href={SOCIALS.MATRIX} target="_blank" rel="noopener noreferrer" aria-label="Matrix">
-          <SiMatrix />
-        </Link>
-        <Link href={SOCIALS.GITHUB} target="_blank" rel="noopener noreferrer" aria-label="GitHub">
-          <FaGithub />
-        </Link>
-      </div>
-    </div>
-  </footer>
-);
+export const Footer: React.FC = () => {
+  const tMisc = useTranslations("misc");
+  const tInfo = useTranslations("info");
+  const tMembership = useTranslations("membership");
+  const tFeatures = useTranslations("features");
+  const tSocials = useTranslations("socials");
 
-export default Footer;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footerTop}>
+        <FooterBrand />
+        <FooterCopyright />
+      </div>
+      <div className={styles.footerBottom}>
+        <div className={styles.footerLinks}>
+          <Link href={ROUTES.CONTACT}>{tMisc("contact")}</Link>
+          <Link href={ROUTES.ABOUT}>{tInfo("about")}</Link>
+          <Link href={ROUTES.TERMS}>{tMisc("terms")}</Link>
+          <Link href={ROUTES.MEMBERSHIP}>{tMembership("premium")}</Link>
+          <Link href={ROUTES.MOBILE_APP}>{tMisc("mobile")}</Link>
+          <Link href={ROUTES.EMBED}>{tFeatures("embed")}</Link>
+          <Link href={ROUTES.DONATE}>{tMisc("donate")}</Link>
+          <Link href={ROUTES.UPDATES}>{tMisc("updates")}</Link>
+        </div>
+        <div className={styles.footerSocialLinks}>
+          <Link href={SOCIALS.DISCORD} target="_blank" rel="noopener noreferrer" aria-label={tSocials("discord")}>
+            <FaDiscord />
+          </Link>
+          <Link href={SOCIALS.ACTIVITY_PUB} target="_blank" rel="noopener noreferrer" aria-label={tSocials("mastodon")}>
+            <FaMastodon />
+          </Link>
+          <Link href={SOCIALS.X} target="_blank" rel="noopener noreferrer" aria-label={tSocials("x")}>
+            <FaXTwitter />
+          </Link>
+          <Link href={SOCIALS.MATRIX} target="_blank" rel="noopener noreferrer" aria-label={tSocials("matrix")}>
+            <SiMatrix />
+          </Link>
+          <Link href={SOCIALS.GITHUB} target="_blank" rel="noopener noreferrer" aria-label={tSocials("github")}>
+            <FaGithub />
+          </Link>
+        </div>
+      </div>
+    </footer>
+  )
+}
