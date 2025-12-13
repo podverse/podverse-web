5fee5901000559bef6ca86e90d4165a73d006e5f
Mitch Downey <mitchldowney@gmail.com>
2025-12-12 21:38:08 -0600

    feat: Add PodcastSettings component for managing podcast feed updates and enhance localization

--- src/app/podcast/[channel_id]/PodcastList.tsx
+++ src/app/podcast/[channel_id]/PodcastList.tsx
@@ -9,6 +9,7 @@ import { ContentAbout } from "../../../components/Content/About/ContentAbout";
 import { ContentPodroll } from "../../../components/Content/Podroll/ContentPodroll";
 import { ListClips } from "../../../components/List/Clips/ListClips";
 import { ListItemSoundbites } from "../../../components/List/ItemSoundbites/ListItemSoundbites";
+import { PodcastSettings } from "../../../components/List/Podcasts/PodcastSettings";
 import styles from "../../../styles/app/podcast/PodcastList.module.scss";
 
 type PodcastListProps = {
@@ -73,6 +74,11 @@ export const PodcastList: React.FC<PodcastListProps> = ({ podroll, ssrChannel })
       {
         type === "podroll" && (
           <ContentPodroll podroll={podroll} />
+        )
+      }
+      {
+        type === "settings" && (
+          <PodcastSettings channel={ssrChannel} />
         )
       }
       <LoadingSpinnerOverlay isLoading={isLoading} />
---
+++ src/components/List/Podcasts/PodcastSettings.tsx
@@ -0,0 +1,60 @@
+import { useLocale, useTranslations } from "use-intl"
+import { Button } from "../../Button/Button";
+import styles from "../../../styles/components/List/Podcasts/PodcastSettings.module.scss";
+import { useAccount } from "../../../contexts/Account";
+import { useModals } from "../../../contexts/Modals";
+import { DTOChannel } from "podverse-helpers";
+import { apiRequestService } from "../../../factories/apiRequestService";
+import { handleRateLimitAlert } from "../../../utils/rateLimit/rateLimitAlert";
+
+type PodcastSettingsProps = {
+  channel: DTOChannel;
+}
+
+export const PodcastSettings = ({ channel }: PodcastSettingsProps) => {
+  const tInfo = useTranslations('info');
+  const tSettings = useTranslations('settings');
+  const tInstructions = useTranslations('instructions');
+  const tMisc = useTranslations('misc');
+  const { loggedInAccount } = useAccount();
+  const { setModalLoginRequired } = useModals();
+  const locale = useLocale();
+
+  const checkFeedForUpdates = async () => {
+    if (!loggedInAccount) {
+      setModalLoginRequired({
+        title: null,
+        message: tInstructions("login_to_add_feeds")
+      })
+      return;
+    }
+
+    if (channel?.feed) {
+      try {
+        await apiRequestService.reqMQRSSAddOnDemand({
+          url: channel.feed.url,
+          podcast_index_id: channel.feed.podcast_index_id
+        });
+        alert(tSettings('podcast.check_feed_added_to_queue'));
+      } catch (error) {
+        const rateLimitErrorHandled = handleRateLimitAlert(error, locale, tMisc);
+        if (!rateLimitErrorHandled) {
+          console.error(error);
+          alert("Error performing action.");
+        }
+      }
+    }
+  }
+
+  return (
+    <div>
+      <h3 className={styles.sectionHeader}>{tInfo('rss_feed')}</h3>
+      <Button
+        variant="primary"
+        onClick={checkFeedForUpdates}
+      >
+        {tSettings('podcast.check_feed_for_updates')}
+      </Button>
+    </div>
+  )
+}
