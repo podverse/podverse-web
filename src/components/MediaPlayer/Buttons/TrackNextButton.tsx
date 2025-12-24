f7d75bde9d780dcf825dbb929b6230b1a2c35857
Mitch Downey <mitchldowney@gmail.com>
2025-12-24 02:03:11 -0600

    fix: Refactor TrackNextButton and TrackNextButtonMobile to use updated media player state

--- src/components/MediaPlayer/Buttons/TrackNextButton.tsx
+++ src/components/MediaPlayer/Buttons/TrackNextButton.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useRef } from "react";
 import { FaForwardStep } from "react-icons/fa6"
 import { useQueueResourcesLoadActive } from "../../../hooks/useQueueResourcesLoadActive";
 import { useQueueResourcesMoveNowPlayingToHistory } from "../../../hooks/useQueueResourceMoveNowPlayingToHistory";
@@ -6,21 +5,15 @@ import { useMediaPlayer } from "../../../contexts/MediaPlayer";
 import styles from "../../../styles/components/MediaPlayer/Buttons/TrackNextButton.module.scss"
 
 export const TrackNextButton = () => {
-  const { mpItem, setMPShouldPlay, mpIsPlaying } = useMediaPlayer();
+  const { mpItem, mpClip, mpItemSoundbite, setMPShouldPlay, mpIsPlaying } = useMediaPlayer();
   const moveNowPlayingToHistory = useQueueResourcesMoveNowPlayingToHistory();
   const queueResourcesLoadActive = useQueueResourcesLoadActive();
 
-  const mpItemRef = useRef(mpItem);
-
-  useEffect(() => {
-    mpItemRef.current = mpItem;
-  }, [mpItem]);
-
   const onClick = async () => {
     await moveNowPlayingToHistory({
-      mpClip: null,
-      mpItem: mpItemRef.current,
-      mpItemSoundbite: null
+      mpClip: mpClip,
+      mpItem: mpItem,
+      mpItemSoundbite: mpItemSoundbite
     });
     setMPShouldPlay(mpIsPlaying);
     await queueResourcesLoadActive();
