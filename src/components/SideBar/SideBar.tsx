1ed954c2481408a1a46760fd9f404eb795dbac4b
Mitch Downey <mitchldowney@gmail.com>
2026-01-14 00:44:02 -0600

    refactor: remove UIThemeToggle component and clean up SideBar references

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -6,7 +6,6 @@ import SideBarDivider from "./SideBarDivider";
 import SideBarBrand from "./SideBarBrand";
 import SideBarLink from "./SideBarLink";
 import SideBarHeader from "./SideBarHeader";
-// import { UIThemeToggle } from "../UIThemeToggle/UIThemeToggle";
 import Accordion from "../Accordian/Accordian";
 import styles from "../../styles/components/SideBar/SideBar.module.scss";
 
@@ -73,8 +72,6 @@ export const SideBar: React.FC = () => {
           size="small"
           open
         />
-        {/* <SideBarDivider /> */}
-        {/* <UIThemeToggle /> */}
       </div>
     </nav>
   );
